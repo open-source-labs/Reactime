1d8e7e93b8ab49e83f2a725d451c688548b32382
Ryan Dang <rydang@umich.edu>
2019-07-23 18:56:41 -0400

    npm package now doesn't receive its own messages

--- package/index.js
+++ package/index.js
@@ -1,7 +1,9 @@
 const snapShot = [];
 
-window.addEventListener('message', (event) => {
-  console.log('data received', event.data);
+window.addEventListener('message', ({ data: { action, payload } }) => {
+  if (action === 'jumpToSnap') {
+    console.log('snap received from chrome', payload);
+  }
 });
 
 const linkState = require('./linkState')(snapShot);
