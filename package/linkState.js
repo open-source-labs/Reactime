564112cbd556d81ebb93140c4953c1168c9cf3a3
Ryan Dang <rydang@umich.edu>
2019-07-24 14:11:40 -0400

    helpful comments

--- package/linkState.js
+++ package/linkState.js
@@ -2,6 +2,8 @@
 // changes the setState method to also update our snapshot
 
 module.exports = (snapShot, mode) => {
+  // send message to window containing component states
+  // unless library is currently jumping through time
   function sendSnapShot() {
     if (mode.jumping) return;
     const payload = snapShot.map(({ component }) => component.state);
@@ -12,21 +14,33 @@ module.exports = (snapShot, mode) => {
   }
 
   return (component) => {
+    // make a copy of setState
     const oldSetState = component.setState.bind(component);
 
+    // convert setState to promise
     const setStateAsync = (state) => {
       return new Promise(resolve => oldSetState(state, resolve));
     };
 
+    // add component to snapshot
     snapShot.push({ component, setStateAsync });
 
+    let first = true;
     function newSetState(state, callback = () => { }) {
+      // if setState is being called for the first time, this conditional sends the initial snapshot
+      if (first) {
+        first = false;
+        sendSnapShot();
+      }
+
+      // continue normal setState functionality, except add sending message middleware
       oldSetState(state, () => {
         sendSnapShot();
         callback();
       });
     }
 
+    // replace component's setState so developer doesn't change syntax
     component.setState = newSetState;
   };
 };
