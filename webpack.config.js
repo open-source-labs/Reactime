f799d7855b3b58fca13c56af0d232678d04bda4d
Josh Kim <joshua940308@gmail.com>
2019-07-26 11:43:41 -0400

    before deleting css

--- webpack.config.js
+++ webpack.config.js
@@ -26,8 +26,8 @@ const config = {
         },
       },
       {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        test: /\.scss$/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
       },
     ],
   },
