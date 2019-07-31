9408dc08113f494a7e1c7cc3c930d2785a7a5418
Josh Kim <joshua940308@gmail.com>
2019-07-31 11:01:14 -0400

    added react-select package

--- src/app/containers/TravelContainer.jsx
+++ src/app/containers/TravelContainer.jsx
@@ -1,48 +1,70 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import Select from 'react-select';
 import MainSlider from '../components/MainSlider';
 
-const TravelContainer = ({
-  moveBackward,
-  moveForward,
-  snapshotsLength,
-  handleChangeSnapshot,
-  handleJumpSnapshot,
-  snapshotIndex,
-  play,
-  playing,
-  playSpeed,
-  pause,
-}) => {
-  const options = ['0.5x', '1.0x', '2.0x'];
-  const defaultOption = options[0];
-  return (
-    <div className="travel-container">
-      <div
-        className="play-button"
-        onClick={() => {
-          play(300);
-          console.log('play clicked');
+class TravelContainer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { playSpeed: 1000 };
+  }
+
+  render() {
+    const { playSpeed } = this.state;
+    const {
+      moveBackward,
+      moveForward,
+      snapshotsLength,
+      handleChangeSnapshot,
+      handleJumpSnapshot,
+      snapshotIndex,
+      play,
+      playing,
+      pause,
+    } = this.props;
+
+    const options = [
+      { value: '2000', label: '0.5x' },
+      { value: '1000', label: '1.0x' },
+      { value: '500', label: '2.0x' },
+    ];
+    const selectedOption = options[0];
+
+    return (
+      <div className="travel-container">
+        <div
+          className="play-button"
+          onClick={() => {
+            play(playSpeed);
+            console.log('play clicked');
+          }}
+        >
+          {playing ? 'Pause' : 'Play'}
+        </div>
+        <MainSlider
+          snapshotLength={snapshotsLength}
+          handleChangeSnapshot={handleChangeSnapshot}
+          snapshotIndex={snapshotIndex}
+          handleJumpSnapshot={handleJumpSnapshot}
+          pause={pause}
+        />
+        <div className="backward-button" role="button" onClick={moveBackward}>
+          {'<'}
+        </div>
+        <div className="forward-button" role="button" onClick={moveForward}>
+          {'>'}
+        </div>
+        {/* <Select
+        value={selectedOption}
+        onChange={() => {
+          console.log('select => value changed');
         }}
-      >
-        {playing ? 'Pause' : 'Play'}
+        options={options}
+      /> */}
       </div>
-      <MainSlider
-        snapshotLength={snapshotsLength}
-        handleChangeSnapshot={handleChangeSnapshot}
-        snapshotIndex={snapshotIndex}
-        handleJumpSnapshot={handleJumpSnapshot}
-        pause={pause}
-      />
-      <div className="backward-button" onClick={moveBackward}>
-        {'<'}
-      </div>
-      <div className="forward-button" onClick={moveForward}>
-        {'>'}
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 TravelContainer.propTypes = {
   pause: PropTypes.func.isRequried,
