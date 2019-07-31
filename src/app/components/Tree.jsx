df3ec945d476369d4c186d57f478ba9644a58f06
Ryan Dang <rydang@umich.edu>
2019-07-31 11:21:23 -0400

    reformatted tree with labels and added props validation

--- src/app/components/Tree.jsx
+++ src/app/components/Tree.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import JSONTree from 'react-json-tree';
+import PropTypes from 'prop-types';
 
 const getItemString = (type, data, itemType, itemString) => (
   <span>
-    {/* //
-    {type} */}
+    {data.name}
   </span>
 );
 
 const Tree = (props) => {
   const { snapshot } = props;
-  console.log('TREE COMPONENT IS PRINTED');
-  console.log(snapshot);
   return (
     <React.Fragment>
       {snapshot && (
@@ -20,11 +18,26 @@ const Tree = (props) => {
           theme={{ tree: () => ({ className: 'json-tree' }) }}
           shouldExpandNode={() => true}
           getItemString={getItemString}
-          // labelRenderer={raw => <strong>{raw}</strong>}
-          // valueRenderer={raw => <em>{raw}</em>}
+          labelRenderer={(raw) => {
+            if (typeof raw[0] !== 'number') return <span>{raw[0]}</span>;
+          }}
         />
       )}
     </React.Fragment>
   );
 };
+
+Tree.propTypes = {
+  snapshot: PropTypes.shape({
+    state: PropTypes.oneOfType([
+      PropTypes.string,
+      PropTypes.object,
+    ]),
+    children: PropTypes.arrayOf(
+      PropTypes.object,
+    ),
+    name: PropTypes.string,
+  }).isRequired,
+};
+
 export default Tree;
