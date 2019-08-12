ae0f68fe2d5221f5c4dff248708721e672aab6b3
Sierra <sierra.swaby@gmail.com>
2019-08-12 15:58:34 -0400

    tab dropdown component created, shows tab options

--- src/app/components/SwitchApp.jsx
+++ src/app/components/SwitchApp.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Select from 'react-select';
-import PropTypes from 'prop-types';
 import { useStoreContext } from '../store';
 
 const SwitchAppDropdown = () => {
@@ -8,12 +7,6 @@ const SwitchAppDropdown = () => {
   const [{ currentTab, tabs }, dispatch] = useStoreContext();
 
   const tabsArray = [];
-
-  // for (let allTabs in tabs){
-  //   tabsArray.push({value: allTabs, label: allTabs})
-  // };
-
-  // console.log(tabsArray)
 
   Object.keys(tabs).forEach((tab) => {
     tabsArray.push({ value: tab, label: tab });
