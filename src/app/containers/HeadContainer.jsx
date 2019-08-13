6b8d685245eb445ed24d372ce7307d6e8c683a1b
Bryan Lee <mylee1995@gmail.com>
2019-08-13 16:08:28 -0400

    headcontainer doesnt use context

--- src/app/containers/HeadContainer.jsx
+++ src/app/containers/HeadContainer.jsx
@@ -1,12 +1,7 @@
 import React from 'react';
 import SwitchAppDropdown from '../components/SwitchApp';
-import { useStoreContext } from '../store';
-
 
 function HeadContainer() {
-  const [store] = useStoreContext();
-  const { tabs, currentTab } = store;
-  const { title } = tabs[currentTab];
   return (
     <div className="head-container">
       <SwitchAppDropdown />
