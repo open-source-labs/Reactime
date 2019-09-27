afbe85d28ea69dcd18885840b169fd5e4a33f744
Ruthba Anam <ruthbaanam@gmail.com>
2019-09-27 14:31:23 -0400

    ported new chart into extension

--- src/app/components/Chart.jsx
+++ src/app/components/Chart.jsx
@@ -10,6 +10,11 @@ let root = {};
 let duration = 750;
 
 class Chart extends Component {
+  constructor() {
+    super();
+    this.chartRef = React.createRef();
+    this.maked3Tree = this.maked3Tree.bind(this);
+  }
   componentDidMount() {
     const { snapshot } = this.props;
     root = JSON.parse(JSON.stringify(snapshot));
@@ -22,232 +27,163 @@ class Chart extends Component {
     this.maked3Tree();
   }
 
-  removed3Tree() {
-    const { anchor } = this.refs;
-    while (anchor.hasChildNodes()) {
-      anchor.removeChild(anchor.lastChild);
-    }
-  }
+  // removed3Tree() {
+  //   const { anchor } = this.refs;
+  //   while (anchor.hasChildNodes()) {
+  //     anchor.removeChild(anchor.lastChild);
+  //   }
+  // }
 
   maked3Tree() {
-    this.removed3Tree();
-    duration = 0;
-
-    const margin = {
-      top: 20,
-      right: 120,
-      bottom: 20,
-      left: 120,
+    // this.removed3Tree();
+    const dataset = {
+      name: "parentMost",
+      children: [
+        {
+          name: 'state1',
+          children: [
+            {
+              name: 'state2',
+              children: [
+                {
+                  name: 'state3',
+                  children: [
+                    {
+                      name: 'state5'
+                    },
+                  ]
+                },
+                {
+                  name: 'state4'
+                },
+                {
+                  name: 'state5'
+                },
+                {
+                  name: 'state6'
+                },
+                {
+                  name: 'state7'
+                },
+                {
+                  name: 'state8'
+                },
+                {
+                  name: 'state9'
+                },
+                {
+                  name: 'state10'
+                },
+                {
+                  name: 'state11'
+                },
+                {
+                  name: 'state12'
+                },
+                {
+                  name: 'state13'
+                },
+                {
+                  name: 'state14'
+                },
+                {
+                  name: 'state15'
+                },
+                {
+                  name: 'state16'
+                },
+                {
+                  name: 'state17'
+                },
+                {
+                  name: 'state18'
+                },
+                {
+                  name: 'state19'
+                },
+              ]
+            },
+          ]
+        },
+      ]
     };
-    // const width = 600 - margin.right - margin.left;
-    const height = 600 - margin.top - margin.bottom;
 
-    let i = 0;
+    let width = 900;
+    let height = 1000;
+    let chartContainer = d3.select(this.chartRef.current)
+      .append('svg') // chartContainer is now pointing to svg
+      .attr('width', width)
+      .attr('height', height);
 
-    const tree = d3.layout
-      .tree()
-      .nodeSize([20])
-      .separation((a, b) => (a.parent === b.parent ? 3 : 1));
-
-    const diagonal = d3.svg.diagonal().projection(d => [d.y, d.x]);
-
-    const svg = d3
-      .select(this.refs.anchor)
-      .append('svg')
-      .attr('width', '100%')
-      .attr('height', '100%')
-      .attr('cursor', '-webkit-grab')
-      .attr('preserveAspectRatio', 'xMinYMin slice')
-      .call(
-        d3.behavior
-          .zoom()
-          .on('zoom', () => svg.attr('transform', `translate(${d3.event.translate}) scale(${d3.event.scale})`)),
-      )
+    const mock = [1, 3, 6, 'haha'];
+    chartContainer.selectAll('g')
+      .data(mock)
+      .enter()
       .append('g')
-      .attr('transform', `translate(60,${height / 2})`);
+      .text(d => 'haha');
 
-    // Add tooltip div
-    const div = d3
-      .select('body')
-      .append('div')
-      .attr('class', 'tooltip')
-      .style('opacity', 1e-6)
-      .on('mouseover', tipMouseover)
-      .on('mouseout', tipMouseout);
+    let g = chartContainer
+      .append("g")
+      .attr("transform", `translate(${width / 2 + 40}, ${height / 2 + 90})`);
+    let radius = width / 2;
 
-    root.x0 = height / 2;
-    root.y0 = 0;
+    let root = d3.hierarchy(dataset);
 
-    function update(source) {
-      // Compute the new tree layout.
-      const nodes = tree.nodes(root).reverse();
-      const links = tree.links(nodes);
+    let treeLayout = d3.tree()
+      .size([2 * Math.PI, radius])
 
-      // Normalize for fixed-depth.
-      nodes.forEach(d => {
-        d.y = d.depth * 180;
+    let tree = treeLayout(root);
+
+    console.log('children', root.descendants());
+
+    g.selectAll('path')
+      .data(root.links())
+      .enter()
+      .append('path')
+      .attr('class', 'link')
+      .attr("d", d3.linkRadial()
+        .angle(d => d.x)
+        .radius(d => d.y));
+    let node = g.selectAll(".node")
+      // root.descendants gets an array of of all nodes
+      .data(root.descendants())
+      .enter()
+      .append("g")
+      .attr("class", function (d) {
+        return "node" + (d.children ? " node--internal" : " node--leaf")
+      })
+      .attr("transform", function (d) {
+        return "translate(" + reinfeldTidierAlgo(d.x, d.y) + ")";
       });
 
-      // Update the nodes…
-      const node = svg.selectAll('g.node').data(nodes, d => {
-        if (!d.id) {
-          i += 1;
-          d.id = i;
-        }
-        return d.id;
+    node.append("circle")
+      .attr("r", 5.5)
+
+    node.append("text")
+      .attr("dy", "0.1em")
+      .attr("x", function (d) {
+        // this positions how far the text is from leaf nodes (ones without children)
+        return d.x < Math.PI === !d.children ? 10 : -10;
+      })
+      .attr("text-anchor", function (d) { return d.x < Math.PI === !d.children ? "start" : "end"; })
+      // this arranges the angle of the text
+      .attr("transform", function (d) { return "rotate(" + (d.x < Math.PI ? d.x - Math.PI / 2 : d.x + Math.PI / 2) * 180 / Math.PI + ")"; })
+      .text(function (d) {
+        return d.data.name
       });
 
-      // Enter any new nodes at the parent's previous position.
-      const nodeEnter = node
-        .enter()
-        .append('g')
-        .attr('class', 'node')
-        .attr('transform', () => `translate(${source.y0},${source.x0})`)
-        .on('click', click)
-        .on('mouseover', mouseover)
-        .on('mouseout', mouseout)
-        .on('mousemove', d => mousemove(d));
-
-      nodeEnter
-        .append('circle')
-        .attr('r', 1e-6)
-        .style('fill', d => (d._children ? 'lightsteelblue' : '#fff'));
-
-      nodeEnter
-        .append('text')
-        .attr('x', d => (d.children || d._children ? -10 : 10))
-        .attr('dy', '.35em')
-        .attr('text-anchor', d => (d.children || d._children ? 'end' : 'start'))
-        .text(d => d.name)
-        .style('fill', '#6FB3D2')
-        .style('fill-opacity', 1e-6);
-
-      // Transition nodes to their new position.
-      const nodeUpdate = node
-        .transition()
-        .duration(duration)
-        .attr('transform', d => `translate(${d.y},${d.x})`);
-
-      nodeUpdate
-        .select('circle')
-        .attr('r', 7)
-        .style('fill', d => (d._children ? '#A1C658' : '#D381C3'));
-
-      nodeUpdate.select('text').style('fill-opacity', 1);
-
-      // Transition exiting nodes to the parent's new position.
-      const nodeExit = node
-        .exit()
-        .transition()
-        .duration(duration)
-        .attr('transform', () => `translate(${source.y},${source.x})`)
-        .remove();
-
-      nodeExit.select('circle').attr('r', 1e-6);
-
-      nodeExit.select('text').style('fill-opacity', 1e-6);
-
-      // Update the links…
-      const link = svg.selectAll('path.link').data(links, d => d.target.id);
-
-      // Enter any new links at the parent's previous position.
-      link
-        .enter()
-        .insert('path', 'g')
-        .attr('class', 'link')
-        .attr('d', () => {
-          const o = { x: source.x0, y: source.y0 };
-          return diagonal({ source: o, target: o });
-        });
-
-      // Transition links to their new position.
-      link
-        .transition()
-        .duration(duration)
-        .attr('d', diagonal);
-
-      // Transition exiting nodes to the parent's new position.
-      link
-        .exit()
-        .transition()
-        .duration(duration)
-        .attr('d', () => {
-          const o = { x: source.x, y: source.y };
-          return diagonal({ source: o, target: o });
-        })
-        .remove();
-
-      // Stash the old positions for transition.
-      nodes.forEach(d => {
-        d.x0 = d.x;
-        d.y0 = d.y;
-      });
+    function reinfeldTidierAlgo(x, y) {
+      return [(y = +y) * Math.cos(x -= Math.PI / 2), y * Math.sin(x)];
     }
-
-    // Toggle children on click.
-    function click(d) {
-      if (d.children) {
-        d._children = d.children;
-        d.children = null;
-      } else {
-        d.children = d._children;
-        d._children = null;
-      }
-      update(d);
-    }
-
-    // Show state on mouse over
-    function mouseover() {
-      div
-        .transition()
-        .duration(300)
-        .style('display', 'block')
-        .style('opacity', 1);
-    }
-
-    function mouseout() {
-      div
-        .transition()
-        .duration(3000)
-        .style('opacity', 1e-6)
-        .style('display', 'none');
-    }
-
-    function tipMouseover() {
-      div
-        .transition()
-        .duration(300)
-        .style('opacity', 1);
-    }
-
-    function tipMouseout() {
-      div
-        .transition()
-        .duration(3000)
-        .style('opacity', 1e-6)
-        .style('display', 'none');
-    }
-
-    function mousemove(d) {
-      div
-        .text(!d.state ? 'No state found' : JSON.stringify(d.state, null, 4))
-        .style('left', `${d3.event.pageX}px`)
-        .style('top', `${d3.event.pageY}px`);
-    }
-
-    update(root);
-    duration = 750;
-
-    // root.children.forEach(collapse);
   }
 
   render() {
-    return <div ref="anchor" className="d3Container" />;
+    return <div ref={this.chartRef} className="d3Container" />;
   }
 }
 
-Chart.propTypes = {
-  snapshot: PropTypes.arrayOf(PropTypes.object).isRequired,
-};
+// Chart.propTypes = {
+//   snapshot: PropTypes.arrayOf(PropTypes.object).isRequired,
+// };
 
 export default Chart;
