e8f91a96486c1dc1f23a2be1baaa73641f248364
Yujin <ykang7858@gmail.com>
2019-10-02 15:12:18 -0400

    test

--- src/app/__tests__/Chart.test.jsx
+++ src/app/__tests__/Chart.test.jsx
@@ -1,6 +1,5 @@
 import React from 'react'; 
 import Chart from '../components/Chart'; 
-import testRunner from 'jest-runner'; 
 
 // Unit test cases for d3 functionality 
 // Test the life cycle methods in Chart
@@ -9,6 +8,7 @@ describe('Life cycle methods in Chart', () => {
     // Component should call maked3Tree upon mounting
     // Use the mock function + .toBeCalled()
    // Test the method in the component 
+   throw new Error(); 
   }); 
 }); 
 
@@ -20,6 +20,7 @@ describe('Root object', () => {
       // i.e.: this.props.hierarchy !== root
       // expect to !== this.props.hierarchy 
       // expect to be a deep clone 
+      throw new Error(); 
   }); 
 }); 
 
@@ -29,6 +30,7 @@ describe('maked3Tree method', () => {
  // Should call function 'removed3tree' only once
     // expect toBeCalled()
     // expect toHaveBeenCalled()
+    throw new Error(); 
   }); 
 }); 
 // Test the tooltip functionality in maked3Tree method 
@@ -37,11 +39,11 @@ describe('tooltip functionality in maked3Tree', () => {
 // a certain event on the tooltip div
   // Should call appropriate function upon triggering mouseOver
   it('should invoke tip.show on mouseOver', () => {
-
+    throw new Error(); 
   }); 
   // Should call appropriate function upon triggering mouseOut
   it ('should invoke tip.hide on mouseOut', () => {
-
+    throw new Error(); 
   }); 
 }); 
 
