422f334c90c825ccfa011fac16991240ef3eed8e
Chris Flannery <chriswillsflannery@gmail.com>
2019-11-04 10:57:56 -0500

    prevented useEffect from crashing on load

--- package/timeJump.js
+++ package/timeJump.js
@@ -28,7 +28,7 @@ module.exports = (origin, mode) => {
       let index = 0;
       const hooks = returnState();
       // while loop through the memoize tree
-      while (current && current.queue) {
+      while (current) {
         current.queue.dispatch(target.state[hooks[index]]);
         // Reassign the current value
         current = current.next;
