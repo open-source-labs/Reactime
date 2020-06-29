6e7a0307c642d76dc5e4553d84ca9af1dbe776d8
crperezt <crperez@gmail.com>
2020-06-29 10:46:45 -0700

    Delete componentActionsRecord.js

--- dev-reactime/componentActionsRecord.js
+++
@@ -1,8 +0,0 @@
-const componentActionsRecord = {};
-const index = 0;
-
-module.exports = {
-  saveNew: (state, setterFunction) => {
-    componentActionsRecord[index] = { state, setterFunction };
-  },
-};
