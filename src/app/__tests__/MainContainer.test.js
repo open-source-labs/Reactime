dc66fbd3f6aa68733c2a036830ffd1654f411ec9
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-07 15:37:54 -0700

    MainContainer.test passing

--- src/app/__tests__/MainContainer.test.js
+++ src/app/__tests__/MainContainer.test.js
@@ -45,7 +45,7 @@ beforeEach(() => {
 describe('MainContainer rendering', () => {
   test('With no snapshots, should not render any containers', () => {
     expect(wrapper.text()).toEqual(
-      'No React application found. Please install our npm package in your app.',
+      'No React application found. Please visit reactime.io to more info.',
     );
     expect(wrapper.find(HeadContainer).length).toBe(0);
     expect(wrapper.find(ActionContainer).length).toBe(0);
