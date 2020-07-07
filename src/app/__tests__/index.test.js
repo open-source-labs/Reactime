ca084a3f4b8b6b6dc2f0eabf4c0dc360866e1277
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-06 23:51:30 -0700

    pass app test

--- src/app/__tests__/index.test.js
+++ src/app/__tests__/index.test.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from '../components/App';
 
 it('renders without crashing', () => {
-  ReactDOM.render(<App />, document.getElementById('root'));
+  const root = document.createElement('root');
+  ReactDOM.render(<App />, root);
 })
