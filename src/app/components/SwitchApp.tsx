033867e8712d1eeb293a4495c7f005a483ad52a3
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-08 12:41:57 -0700

    added SwitchApp.tsx

---
+++ src/app/components/SwitchApp.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import Select from 'react-select';
+import { useStoreContext } from '../store.tsx';
+import { setTab } from '../actions/actions.tsx';
+
+
+const SwitchAppDropdown = () => {
+  const [{ currentTab, tabs }, dispatch] = useStoreContext();
+  
+  const tabsArray = [];
+  Object.keys(tabs).forEach(tab => {
+    tabsArray.unshift({ value: tab, label: tabs[tab].title });
+  });
+
+  const currTab = {
+    value: currentTab,
+    label: tabs[currentTab].title,
+  };
+
+  return (
+    <Select
+      className="tab-select-container"
+      classNamePrefix="tab-select"
+      value={currTab}
+      onChange={e => {
+        dispatch(setTab(parseInt(e.value, 10)));
+      }}
+      options={tabsArray}
+    />
+  );
+};
+
+export default SwitchAppDropdown;
