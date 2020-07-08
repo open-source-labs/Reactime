3452fd2344c59877c91d9e65b863ba6142f42321
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-08 12:36:52 -0700

    added MainSlider.tsx

---
+++ src/app/components/MainSlider.tsx
@@ -0,0 +1,54 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/prop-types */
+import React from 'react';
+import Slider from 'rc-slider';
+import Tooltip from 'rc-tooltip';
+import PropTypes from 'prop-types';
+
+import { changeSlider, pause } from '../actions/actions';
+import { useStoreContext } from '../store';
+
+const { Handle } = Slider;
+
+const handle = props => {
+  const {
+    value, dragging, index, ...restProps
+  } = props;
+
+  return (
+    <Tooltip
+      prefixCls="rc-slider-tooltip"
+      overlay={value}
+      visible={dragging}
+      placement="top"
+      key={index}
+    >
+      <Handle value={value} {...restProps} />
+    </Tooltip>
+  );
+};
+
+function MainSlider({ snapshotsLength }) {
+  const [{ tabs, currentTab }, dispatch] = useStoreContext();
+  const { sliderIndex } = tabs[currentTab];
+
+  return (
+    <Slider
+      min={0}
+      max={snapshotsLength - 1}
+      value={sliderIndex}
+      onChange={index => {
+        const newIndex = index === -1 ? 0 : index;
+        dispatch(changeSlider(newIndex));
+        dispatch(pause());
+      }}
+      handle={handle}
+    />
+  );
+}
+
+MainSlider.propTypes = {
+  snapshotsLength: PropTypes.number.isRequired,
+};
+
+export default MainSlider;
