40fd8fa67e38eeeaffc19a3cc40f608506d12e4e
Nathanael Wa Mwenze <11463376+nmwenz90@users.noreply.github.com>
2020-07-09 16:31:29 -0700

    modified DiffRoute test jsx

--- src/app/__tests__/DiffRoute.test.jsx
+++ src/app/__tests__/DiffRoute.test.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import { configure, shallow } from 'enzyme'
+import { configure, shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
 import {
   MemoryRouter as Router, Route, NavLink, Switch,
 } from 'react-router-dom';
-import { createBrowserHistory, createMemoryHistory } from 'history';
 
 import DiffRoute from '../components/DiffRoute.jsx';
 
@@ -21,16 +20,15 @@ describe('DiffRoute props', () => {
     expect(props).toHaveProperty('snapshot');
   });
   it('props snapshot value should be an array', () => {
-    expect(Array.isArray(props.snapshot)).toBe(true)
+    expect(Array.isArray(props.snapshot)).toBe(true);
   });
 });
 
 describe('DiffRoute component', () => {
   beforeEach(() => {
-    wrapper = shallow(<DiffRoute {...props} />);
+    wrapper = shallow(<DiffRoute />);
   });
   it('should contain a router component', () => {
-
     expect(wrapper.find(Router).type()).toEqual(Router);
   });
   it('div tag in Router should have a classname "navbar', () => {
@@ -41,7 +39,7 @@ describe('DiffRoute component', () => {
   });
 });
 
-
-// check if router component has a dive with a navlik component
-// check if navlinks go to appropriate routes
+// remaining tests:
+// check if router component has a div with a navlik component
+// check if navlinks go to appropriate routes, and text shows Tree and Raw
 // check if routes in switch have appropriate props
