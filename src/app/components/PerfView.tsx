5aa7178c5c35612e889a323a61887055bd721d5c
Greg Panciera <gregpanciera@gmail.com>
2020-07-11 18:53:07 -0500

    add PerfView.test; update font size in PerfView; adjust chart colors

--- src/app/components/PerfView.tsx
+++ src/app/components/PerfView.tsx
@@ -41,8 +41,8 @@ const PerfView = (props:PerfViewProps) => {
   // Set up color scaling function
   const colorScale = d3.scaleLinear()
     .domain([0, 7])
-    .range(['hsl(152,30%,80%)', 'hsl(228,30%,40%)'])
-    // .range(['hsl(210,30%,80%)', 'hsl(152,30%,40%)'])
+    .range(['hsl(200,60%,60%)', 'hsl(255,30%,30%)'])
+    // .range(['hsl(152,30%,80%)', 'hsl(228,30%,40%)'])
     .interpolate(d3.interpolateHcl);
 
   // Set up circle-packing layout function
