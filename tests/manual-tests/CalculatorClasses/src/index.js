582b133bbf38ad4e8153715ea8c9995d415a98e4
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-13 12:27:18 -0700

    added calculator to manual tests

---
+++ tests/manual-tests/CalculatorClasses/src/index.js
@@ -0,0 +1,14 @@
+/* eslint-disable import/no-named-as-default */
+/* eslint-disable import/no-named-as-default-member */
+/* eslint-disable no-unused-vars */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reactime from 'reactime';
+import App from './component/App';
+import './index.css';
+import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
+// gabi :: uncomment the line below to test Reactime 3.3 or older 
+// reactime(document.getElementById("root"));
