baf1a6e4e279bc18ad6bf78136b4642021ca5268
aquinojardim <64516365+aquinojardim@users.noreply.github.com>
2020-07-13 13:18:42 -0700

    added more manual tests

--- tests/manual-tests/CalculatorClasses/src/index.js
+++ tests/manual-tests/CalculatorClasses/src/index.js
@@ -1,14 +1,9 @@
-/* eslint-disable import/no-named-as-default */
-/* eslint-disable import/no-named-as-default-member */
-/* eslint-disable no-unused-vars */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import reactime from 'reactime';
-import App from './component/App';
-import './index.css';
-import 'github-fork-ribbon-css/gh-fork-ribbon.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import reactime from "reactime";
+import App from "./component/App";
+import "./index.css";
+import "github-fork-ribbon-css/gh-fork-ribbon.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
-
-// gabi :: uncomment the line below to test Reactime 3.3 or older 
+ReactDOM.render(<App />, document.getElementById("root"));
 // reactime(document.getElementById("root"));
