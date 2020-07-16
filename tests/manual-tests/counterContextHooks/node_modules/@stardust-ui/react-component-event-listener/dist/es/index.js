3258d8cabc1d0c132d2d0e3dc4dd93fe7f1724d4
crperezt <crperez@gmail.com>
2020-07-16 11:38:51 -0700

    removed node_modules from repo

--- tests/manual-tests/counterContextHooks/node_modules/@stardust-ui/react-component-event-listener/dist/es/index.js
+++
@@ -1,9 +0,0 @@
-export var documentRef = {
-  current: typeof document === 'undefined' ? null : document
-};
-export var windowRef = {
-  current: typeof window === 'undefined' ? null : window
-};
-export { default as EventListener } from './EventListener';
-export * from './types';
-export { default as useEventListener } from './useEventListener';
--- tests/manual-tests/counterContextHooks/node_modules/@stardust-ui/react-component-ref/dist/es/toRefObject.d.ts
+++
@@ -1,4 +0,0 @@
-import * as React from 'react';
-/** Creates a React ref object from existing DOM node. */
-declare const toRefObject: <T extends Node>(node: T) => React.RefObject<T>;
-export default toRefObject;
