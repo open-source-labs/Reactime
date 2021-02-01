3ef3429b6a3165b5aae505aee3f4ce97f2fc533a
Ali <marchofthepenguins1@gmail.com>
2021-02-01 15:07:31 -0800

    beta version

--- src/app/components/WebMetrics.tsx
+++ src/app/components/WebMetrics.tsx
@@ -1,104 +1,120 @@
-import React, { useState, useEffect, Component } from 'react';
-import Charts from 'react-apexcharts';
-import useForceUpdate from './useForceUpdate';
+import React, { useState, useEffect, Component } from "react";
+import Charts from "react-apexcharts";
+import useForceUpdate from "./useForceUpdate";
 
 const radialGraph = (props) => {
+  const [hoverView, setHoverView] = useState(false);
+  const state = {
+    series: [props.series],
+    options: {
+      colors: [props.color],
+      chart: {
+        height: 100,
+        width: 100,
+        type: "radialBar",
+        toolbar: {
+          show: false,
+        },
+      },
+      plotOptions: {
+        radialBar: {
+          startAngle: -135,
+          endAngle: 135,
+          hollow: {
+            margin: 0,
+            size: "75%",
+            background: "#242529",
+            image: undefined,
+            imageOffsetX: 0,
+            imageOffsetY: 0,
+            position: "front",
+            dropShadow: {
+              enabled: false,
+              top: 3,
+              left: 0,
+              blur: 4,
+              opacity: 0.24,
+            },
+          },
+          track: {
+            background: "#fff",
+            strokeWidth: "3%",
+            margin: 0, // margin is in pixels
+            dropShadow: {
+              enabled: true,
+              top: -3,
+              left: 0,
+              blur: 4,
+              opacity: 0.35,
+            },
+          },
 
-		const state = {
-		
-			series: [props.series],
-			options: {
-				colors: [props.color],
-				chart: {
-					height: 100,
-					width: 100,
-					type: 'radialBar',
-					toolbar: {
-						show: false,
-					}
-				},
-				plotOptions: {
-					radialBar: {
-						startAngle: -135,
-						endAngle: 135,
-						 hollow: {
-							margin: 0,
-							size: '75%',
-							background: '#242529',
-							image: undefined,
-							imageOffsetX: 0,
-							imageOffsetY: 0,
-							position: 'front',
-							dropShadow: {
-								enabled: false,
-								top: 3,
-								left: 0,
-								blur: 4,
-								opacity: 0.24
-							}
-						},
-						track: {
-							background: '#fff',
-							strokeWidth: '3%',
-							margin: 0, // margin is in pixels
-							dropShadow: {
-								enabled: true,
-								top: -3,
-								left: 0,
-								blur: 4,
-								opacity: 0.35
-							}
-						},
-				
-						dataLabels: {
-							show: true,
-							name: {
-								offsetY: -10,
-								show: true,
-								color: '#fff',
-								fontSize: '24px'
-							},
-							value: {
-								formatter: props.formatted,
-								color: '#fff',
-								fontSize: '16px',
-								show: true,
-							}
-						}
-					}
-				},
-				fill: {
-					type: 'solid',
-					gradient: {
-						shade: 'dark',
-						type: 'horizontal',
-						shadeIntensity: 0.1,
-						gradientToColors: [props.color],
-						inverseColors: false,
-						opacityFrom: 1,
-						opacityTo: 1,
-						stops: [0, 100]
-					}
-				},
-				stroke: {
-					lineCap: 'flat'
-				},
-				labels: [props.label],
-			},		
-    };
+          dataLabels: {
+            show: true,
+            name: {
+              offsetY: -10,
+              show: true,
+              color: "#fff",
+              fontSize: "24px",
+            },
+            value: {
+              formatter: props.formatted,
+              color: "#fff",
+              fontSize: "16px",
+              show: true,
+            },
+          },
+        },
+      },
+      fill: {
+        type: "solid",
+        gradient: {
+          shade: "dark",
+          type: "horizontal",
+          shadeIntensity: 0.1,
+          gradientToColors: [props.color],
+          inverseColors: false,
+          opacityFrom: 1,
+          opacityTo: 1,
+          stops: [0, 100],
+        },
+      },
+      stroke: {
+        lineCap: "flat",
+      },
+      labels: [props.label],
+    },
+  };
+  //   const changeHoverText = () => {
+  // 	  setHoverView(true);
+  // 	// 	const hoverBoxElem = document.getElementById("hoverText");
+  //     // hoverBoxElem.innerHTML = `${state.series}`;
+  //   };
 
-
-		return (
-			
-
-<div id="card" className="metric">
-	<div id="chart">
-		<Charts options={state.options} series={state.series} type="radialBar" height={200} width={200}/>
-	</div>
-</div>
-
-		)
-}
-
+  return (
+    <div className="metric">
+      <div
+        id="chart"
+        onMouseEnter={() => setHoverView(true)}
+        onMouseLeave={() => setHoverView(false)}
+      >
+        <Charts
+          options={state.options}
+          series={state.series}
+          type="radialBar"
+          height={200}
+          width={200}
+        />
+        {hoverView ? (
+          <p>
+            {state.series} "Lorem ipsum dolor sit amet, consectetur adipiscing
+            elit, sed do eiusmod tempor incididunt ut labore et dolore magna
+            aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
+          </p>
+        ) : null}
+      </div>
+    </div>
+  );
+};
 
 export default radialGraph;
