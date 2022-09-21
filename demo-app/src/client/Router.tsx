4a95b09aeb973824a48c1a24e6fcbac4e8cba16c
yceffort <yc.effort@navercorp.com>
2022-09-21 23:44:25 +0900

    :package: update @types/react, @types/react-dom
    :hammer: update old renderer
    
    :bug: fix double quotation mark
    
    :bug: add missing semicolon

--- demo-app/src/client/Router.tsx
+++ demo-app/src/client/Router.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Nav from "./Components/Nav";
 import Board from "./Components/Board";
 import Home from "./Components/Home";
 import Buttons from "./Components/Buttons";
 
-const root = document.getElementById("root");
+const app = document.getElementById("root");
+const root = createRoot(app); 
 
-ReactDOM.render(
-    <BrowserRouter>
-      <Nav />
-      <Routes>
-        <Route path="/tictactoe" element={<Board />}/>
-        <Route path="/" element={<Home />}/>
-        <Route path="/buttons" element={<Buttons />}/>
-      </Routes>
-    </BrowserRouter>, root);
+root.render(<BrowserRouter>
+  <Nav />
+  <Routes>
+    <Route path="/tictactoe" element={<Board />}/>
+    <Route path="/" element={<Home />}/>
+    <Route path="/buttons" element={<Buttons />}/>
+  </Routes>
+</BrowserRouter>);
