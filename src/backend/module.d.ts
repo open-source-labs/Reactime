492572c42ad6023ab058d02469773fd12f1125e8
Emin Tahirov <emintahirov1996@gmail.com>
2022-10-12 18:27:34 -0600

    Deleting commented out import core.js on Tree.ts

--- src/backend/module.d.ts
+++ src/backend/module.d.ts
@@ -4,7 +4,15 @@
 // can run on outdated platforms and with outdated applications
 declare module 'core-js';
 // Regenerator runtime provides runtime support for compiled/transpiled async functions.
-
+// Like babel that compiles modern js into older js,
+// async functions are also compiled to run on engines that don't support async.
+// After babel does the syntax transformation or transpiles the async functions,
+// the resulting code uses regen runtime to run
+// https://stackoverflow.com/questions/65378542/what-is-regenerator-runtime-npm-package-used-for
 declare module 'regenerator-runtime/runtime';
+// Acorn is a jsx parser that was experimental to be faster than than the react.js jsx parser,
+// however is not maintained at this time, last update was more than one year ago 10-5-21
 declare module 'acorn-jsx';
+// Traspiler to traspile code to JSX AST but to use with regular es5 compliant js,
+// still needs babel and buble trasnpilers which use acorn-jsx under the hood
 declare module 'acorn';
