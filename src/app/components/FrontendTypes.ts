5849d544c7bc8db8722bb8a78f12903ea43d022f
louislam <louislam7229@gmail.com>
2022-10-17 15:19:50 -0700

    added nocheck for in progress file and comparison type to string temporarily

--- src/app/components/FrontendTypes.ts
+++ src/app/components/FrontendTypes.ts
@@ -57,7 +57,7 @@ export interface BarGraphBase {
   width: number,
   height: number,
   data: PerfData,
-  comparison: Series[],
+  comparison: string | Series[],
 }
 
 export interface BarGraphComparisonProps extends BarGraphBase {
--- src/app/components/StateRoute/PerformanceVisx/BarGraph.tsx
+++ src/app/components/StateRoute/PerformanceVisx/BarGraph.tsx
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import React, { useEffect, useState } from 'react';
 import { BarStack } from '@visx/shape';
 import { Group } from '@visx/group';
