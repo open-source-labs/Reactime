09336d0266d641df6068a72ae5884654be9ede40
benmarg <benjaminmargolius@gmail.com>
2022-12-12 14:39:38 -0800

    initial nav bar, LandingPage, and TeamSection added

--- www/src/pages/components/LandingPage.tsx
+++ www/src/pages/components/LandingPage.tsx
@@ -1,27 +1,5 @@
-/*
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  const colors = require('tailwindcss/colors')
-  
-  module.exports = {
-    // ...
-    theme: {
-      extend: {
-        colors: {
-          rose: colors.rose,
-        },
-      },
-    },
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-*/
 import { ChevronRightIcon, StarIcon } from '@heroicons/react/20/solid'
+import TeamSection from '../components/TeamSection'
 
 const stats = [
   { label: 'Founded', value: '2021' },
@@ -117,13 +95,13 @@ export default function LandingPage() {
       <main>
         {/* Hero section */}
         <div className="overflow-hidden pt-8 sm:pt-12 lg:relative lg:py-48">
-          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-24 lg:px-8">
+          <div className="mx-32 max-w-md px-4 sm:max-w-3xl sm:px-6 lg:grid lg:max-w-7xl lg:grid-cols-2 lg:gap-24 lg:px-8">
             <div>
               <div>
                 <img
-                  className="pl-52 h-20 w-auto"
+                  className="pl-60 h-20 w-auto"
                   src="https://i.imgur.com/ELBAyVb.png"
-                  alt="Your Company"
+                  alt="Reactime"
                 />
               </div>
               <div className="mt-20">
@@ -143,7 +121,7 @@ export default function LandingPage() {
                     A time travel debugger for modern react apps
                   </h1>
                   <p className="mt-6 text-xl text-gray-500">
-                    Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.
+                    A Chrome Extension that lets you rewind time and replay previous versions of your stateful React components.
                   </p>
                 </div>
                 <form action="#" className="mt-12 sm:flex sm:w-full sm:max-w-lg">
@@ -168,17 +146,15 @@ export default function LandingPage() {
                   </div>
                 </form>
                 <div className="mt-6">
-                  <div className="inline-flex items-center divide-x divide-gray-300">
-                    <div className="flex flex-shrink-0 pr-5">
-                      <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
-                      <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
-                      <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
-                      <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
-                      <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" />
+                  <div className="inline-flex items-center">
+                    <div className="flex flex-shrink-0">
+                      {/* <StarIcon className="h-5 w-5 text-yellow-400" aria-hidden="true" /> */}
                     </div>
-                    <div className="min-w-0 flex-1 py-1 pl-5 text-sm text-gray-500 sm:py-3">
-                      <span className="font-medium text-gray-900">Rated 5 stars</span> by over{' '}
-                      <span className="font-medium text-rose-500">500 beta users</span>
+                    <div className="min-w-0 inline py-1 text-sm text-gray-500 sm:py-3">
+                    <StarIcon className="h-5 w-5 mb-1 mr-2 text-yellow-400 inline" aria-hidden="true" />
+                      <span className="font-medium text-gray-900"> Starred on GitHub</span> by over{' '}
+                      <span className="font-medium text-rose-500">1,700 users</span>
+                      <StarIcon className="h-5 w-5 ml-2 mb-1 text-yellow-400 inline" aria-hidden="true" />
                     </div>
                   </div>
                 </div>
@@ -223,230 +199,11 @@ export default function LandingPage() {
           </div>
         </div>
 
-        {/* Testimonial/stats section */}
-        <div className="relative mt-20">
-          <div className="lg:mx-auto lg:grid lg:max-w-7xl lg:grid-cols-2 lg:items-start lg:gap-24 lg:px-8">
-            <div className="relative sm:py-16 lg:py-0">
-              <div aria-hidden="true" className="hidden sm:block lg:absolute lg:inset-y-0 lg:right-0 lg:w-screen">
-                <div className="absolute inset-y-0 right-1/2 w-full rounded-r-3xl bg-gray-50 lg:right-72" />
-                <svg
-                  className="absolute top-8 left-1/2 -ml-3 lg:-right-8 lg:left-auto lg:top-12"
-                  width={404}
-                  height={392}
-                  fill="none"
-                  viewBox="0 0 404 392"
-                >
-                  <defs>
-                    <pattern
-                      id="02f20b47-fd69-4224-a62a-4c9de5c763f7"
-                      x={0}
-                      y={0}
-                      width={20}
-                      height={20}
-                      patternUnits="userSpaceOnUse"
-                    >
-                      <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                    </pattern>
-                  </defs>
-                  <rect width={404} height={392} fill="url(#02f20b47-fd69-4224-a62a-4c9de5c763f7)" />
-                </svg>
-              </div>
-              <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:px-0 lg:py-20">
-                {/* Testimonial card*/}
-                <div className="relative overflow-hidden rounded-2xl pt-64 pb-10 shadow-xl">
-                  <img
-                    className="absolute inset-0 h-full w-full object-cover"
-                    src="https://images.unsplash.com/photo-1521510895919-46920266ddb3?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&fp-x=0.5&fp-y=0.6&fp-z=3&width=1440&height=1440&sat=-100"
-                    alt=""
-                  />
-                  <div className="absolute inset-0 bg-rose-500 mix-blend-multiply" />
-                  <div className="absolute inset-0 bg-gradient-to-t from-rose-600 via-rose-600 opacity-90" />
-                  <div className="relative px-8">
-                    <div>
-                      <img
-                        className="h-12"
-                        src="https://tailwindui.com/img/logos/workcation.svg?color=white"
-                        alt="Workcation"
-                      />
-                    </div>
-                    <blockquote className="mt-8">
-                      <div className="relative text-lg font-medium text-white md:flex-grow">
-                        <svg
-                          className="absolute top-0 left-0 h-8 w-8 -translate-x-3 -translate-y-2 transform text-rose-400"
-                          fill="currentColor"
-                          viewBox="0 0 32 32"
-                          aria-hidden="true"
-                        >
-                          <path d="M9.352 4C4.456 7.456 1 13.12 1 19.36c0 5.088 3.072 8.064 6.624 8.064 3.36 0 5.856-2.688 5.856-5.856 0-3.168-2.208-5.472-5.088-5.472-.576 0-1.344.096-1.536.192.48-3.264 3.552-7.104 6.624-9.024L9.352 4zm16.512 0c-4.8 3.456-8.256 9.12-8.256 15.36 0 5.088 3.072 8.064 6.624 8.064 3.264 0 5.856-2.688 5.856-5.856 0-3.168-2.304-5.472-5.184-5.472-.576 0-1.248.096-1.44.192.48-3.264 3.456-7.104 6.528-9.024L25.864 4z" />
-                        </svg>
-                        <p className="relative">
-                          Tincidunt integer commodo, cursus etiam aliquam neque, et. Consectetur pretium in volutpat,
-                          diam. Montes, magna cursus nulla feugiat dignissim id lobortis amet.
-                        </p>
-                      </div>
+      <div className="relative mt-20">
+        <TeamSection />
+      </div>
 
-                      <footer className="mt-4">
-                        <p className="text-base font-semibold text-rose-200">Sarah Williams, CEO at Workcation</p>
-                      </footer>
-                    </blockquote>
-                  </div>
-                </div>
-              </div>
-            </div>
 
-            <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:px-0">
-              {/* Content area */}
-              <div className="pt-12 sm:pt-16 lg:pt-20">
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  On a mission to empower teams
-                </h2>
-                <div className="mt-6 space-y-6 text-gray-500">
-                  <p className="text-lg">
-                    Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque
-                    tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus
-                    morbi vel porttitor vitae ut. Amet vitae fames senectus vitae.
-                  </p>
-                  <p className="text-base leading-7">
-                    Sollicitudin tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget
-                    pretium arcu vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna. Sollicitudin
-                    tristique eros erat odio sed vitae, consequat turpis elementum. Lorem nibh vel, eget pretium arcu
-                    vitae. Eros eu viverra donec ut volutpat donec laoreet quam urna.
-                  </p>
-                  <p className="text-base leading-7">
-                    Rhoncus nisl, libero egestas diam fermentum dui. At quis tincidunt vel ultricies. Vulputate aliquet
-                    velit faucibus semper. Pellentesque in venenatis vestibulum consectetur nibh id. In id ut tempus
-                    egestas. Enim sit aliquam nec, a. Morbi enim fermentum lacus in. Viverra.
-                  </p>
-                </div>
-              </div>
-
-              {/* Stats section */}
-              <div className="mt-10">
-                <dl className="grid grid-cols-2 gap-x-4 gap-y-8">
-                  {stats.map((stat) => (
-                    <div key={stat.label} className="border-t-2 border-gray-100 pt-6">
-                      <dt className="text-base font-medium text-gray-500">{stat.label}</dt>
-                      <dd className="text-3xl font-bold tracking-tight text-gray-900">{stat.value}</dd>
-                    </div>
-                  ))}
-                </dl>
-                <div className="mt-10">
-                  <a href="#" className="text-base font-medium text-rose-500">
-                    Learn more about how we're changing the world&nbsp&rarr;
-                  </a>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* Logo cloud section */}
-        <div className="mt-32">
-          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="lg:grid lg:grid-cols-2 lg:items-center lg:gap-24">
-              <div>
-                <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-                  Backed by world-renowned investors
-                </h2>
-                <p className="mt-6 max-w-3xl text-lg leading-7 text-gray-500">
-                  Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque
-                  tristique pellentesque. Blandit amet, sed aenean erat arcu morbi. Cursus faucibus nunc nisl netus
-                  morbi vel porttitor vitae ut. Amet vitae fames senectus vitae.
-                </p>
-                <div className="mt-6">
-                  <a href="#" className="text-base font-medium text-rose-500">
-                    Meet our investors and advisors&nbsp&rarr;
-                  </a>
-                </div>
-              </div>
-              <div className="mt-12 grid grid-cols-2 gap-0.5 md:grid-cols-3 lg:mt-0 lg:grid-cols-2">
-                {logos.map((logo) => (
-                  <div key={logo.name} className="col-span-1 flex justify-center bg-gray-50 py-8 px-8">
-                    <img className="max-h-12" src={logo.url} alt={logo.name} />
-                  </div>
-                ))}
-              </div>
-            </div>
-          </div>
-        </div>
-
-        {/* CTA section */}
-        <div className="relative mt-24 sm:mt-32 sm:py-16">
-          <div aria-hidden="true" className="hidden sm:block">
-            <div className="absolute inset-y-0 left-0 w-1/2 rounded-r-3xl bg-gray-50" />
-            <svg className="absolute top-8 left-1/2 -ml-3" width={404} height={392} fill="none" viewBox="0 0 404 392">
-              <defs>
-                <pattern
-                  id="8228f071-bcee-4ec8-905a-2a059a2cc4fb"
-                  x={0}
-                  y={0}
-                  width={20}
-                  height={20}
-                  patternUnits="userSpaceOnUse"
-                >
-                  <rect x={0} y={0} width={4} height={4} className="text-gray-200" fill="currentColor" />
-                </pattern>
-              </defs>
-              <rect width={404} height={392} fill="url(#8228f071-bcee-4ec8-905a-2a059a2cc4fb)" />
-            </svg>
-          </div>
-          <div className="mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-7xl lg:px-8">
-            <div className="relative overflow-hidden rounded-2xl bg-rose-500 px-6 py-10 shadow-xl sm:px-12 sm:py-20">
-              <div aria-hidden="true" className="absolute inset-0 -mt-72 sm:-mt-32 md:mt-0">
-                <svg
-                  className="absolute inset-0 h-full w-full"
-                  preserveAspectRatio="xMidYMid slice"
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 1463 360"
-                >
-                  <path
-                    className="text-rose-400 text-opacity-40"
-                    fill="currentColor"
-                    d="M-82.673 72l1761.849 472.086-134.327 501.315-1761.85-472.086z"
-                  />
-                  <path
-                    className="text-rose-600 text-opacity-40"
-                    fill="currentColor"
-                    d="M-217.088 544.086L1544.761 72l134.327 501.316-1761.849 472.086z"
-                  />
-                </svg>
-              </div>
-              <div className="relative">
-                <div className="sm:text-center">
-                  <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                    Get notified when we&rsquo;re launching.
-                  </h2>
-                  <p className="mx-auto mt-6 max-w-2xl text-lg text-rose-100">
-                    Sagittis scelerisque nulla cursus in enim consectetur quam. Dictum urna sed consectetur neque
-                    tristique pellentesque.
-                  </p>
-                </div>
-                <form action="#" className="mt-12 sm:mx-auto sm:flex sm:max-w-lg">
-                  <div className="min-w-0 flex-1">
-                    <label htmlFor="cta-email" className="sr-only">
-                      Email address
-                    </label>
-                    <input
-                      id="cta-email"
-                      type="email"
-                      className="block w-full rounded-md border border-transparent px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-transparent focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-rose-500"
-                      placeholder="Enter your email"
-                    />
-                  </div>
-                  <div className="mt-4 sm:mt-0 sm:ml-3">
-                    <button
-                      type="submit"
-                      className="block w-full rounded-md border border-transparent bg-gray-900 px-5 py-3 text-base font-medium text-white shadow hover:bg-black focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-rose-500 sm:px-10"
-                    >
-                      Notify me
-                    </button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-        </div>
       </main>
 
       {/* Footer section */}
--- www/src/pages/components/NavBar.jsx
+++ www/src/pages/components/NavBar.jsx
@@ -11,22 +11,28 @@ export default function NavBar() {
     <Disclosure as="nav" className="bg-[#333333]">
       {({ open }) => (
         <>
-          <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-            <div className="flex h-16 items-center justify-between">
+          <div className="mx-auto max-w9xl px-4 sm:px-6 lg:px-8">
+            <div className="flex h-16 mx-6 items-center justify-between">
               <div className="flex items-center">
-                <div className="flex-shrink-0">
+                <div className="ml-6 flex-shrink-0">
                   <img
                     className="block h-8 w-auto lg:hidden"
                     src="https://i.imgur.com/ELBAyVb.png"
                     alt="Your Company"
                   />
                   <img
-                    className="hidden h-8 w-auto lg:block"
+                    className="hidden h-8 w-auto lg:block invert"
                     src="https://i.imgur.com/ELBAyVb.png"
                     alt="Your Company"
                   />
                 </div>
-                <div className="hidden sm:ml-6 sm:block">
+                <span className="inline-flex items-center mx-4 rounded-full bg-red-200 px-2.5 py-0.5 text-xs font-medium text-red-800">
+                  v17.0.0
+                </span>
+                
+            </div>
+
+            <div className="hidden sm:ml-6 sm:block">
                   <div className="flex space-x-4">
                     {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
                     <a href="#" className="rounded-md bg-gray-900 px-3 py-2 text-sm font-medium text-white">
@@ -52,95 +58,7 @@ export default function NavBar() {
                     </a>
                   </div>
                 </div>
-              </div>
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex items-center">
-                  <button
-                    type="button"
-                    className="rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-                  >
-                    <span className="sr-only">View notifications</span>
-                    <BellIcon className="h-6 w-6" aria-hidden="true" />
-                  </button>
-
-                  {/* Profile dropdown */}
-                  <Menu as="div" className="relative ml-3">
-                    <div>
-                      <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
-                        <span className="sr-only">Open user menu</span>
-                        <img
-                          className="h-8 w-8 rounded-full"
-                          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                          alt=""
-                        />
-                      </Menu.Button>
-                    </div>
-                    <Transition
-                      as={Fragment}
-                      enter="transition ease-out duration-100"
-                      enterFrom="transform opacity-0 scale-95"
-                      enterTo="transform opacity-100 scale-100"
-                      leave="transition ease-in duration-75"
-                      leaveFrom="transform opacity-100 scale-100"
-                      leaveTo="transform opacity-0 scale-95"
-                    >
-                      <Menu.Items className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Your Profile
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Settings
-                            </a>
-                          )}
-                        </Menu.Item>
-                        <Menu.Item>
-                          {({ active }) => (
-                            <a
-                              href="#"
-                              className={classNames(
-                                active ? 'bg-gray-100' : '',
-                                'block px-4 py-2 text-sm text-gray-700'
-                              )}
-                            >
-                              Sign out
-                            </a>
-                          )}
-                        </Menu.Item>
-                      </Menu.Items>
-                    </Transition>
-                  </Menu>
-                </div>
-              </div>
-              <div className="-mr-2 flex sm:hidden">
-                {/* Mobile menu button */}
-                <Disclosure.Button className="inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
-                  <span className="sr-only">Open main menu</span>
-                  {open ? (
-                    <XMarkIcon className="block h-6 w-6" aria-hidden="true" />
-                  ) : (
-                    <Bars3Icon className="block h-6 w-6" aria-hidden="true" />
-                  )}
-                </Disclosure.Button>
-              </div>
-            </div>
+              </div>  
           </div>
 
           <Disclosure.Panel className="sm:hidden">
