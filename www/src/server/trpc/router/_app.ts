9a431f3ba8852d5c9942d000cbd449d13b9a0fa0
jemzir <nghiemj23@gmail.com>
2022-12-19 15:20:19 -0800

    created backend for router and user schema via prisma
    Coauthored: James Nghiem, Benjamin Margolius, Eric Yun, Wilton Lee

--- www/src/server/trpc/router/_app.ts
+++ www/src/server/trpc/router/_app.ts
@@ -1,8 +1,8 @@
 import { router } from "../trpc";
-import { exampleRouter } from "./example";
+import { userRouter } from "./user";
 
 export const appRouter = router({
-  example: exampleRouter,
+  user : userRouter,
 });
 
 // export type definition of API
