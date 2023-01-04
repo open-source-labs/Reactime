2ff43879ff9326ea6fcbc086efd88c4f21cf0041
wiltonlee948 <wiltonlee948@gmail.com>
2023-01-03 20:37:27 -0800

    created conditional rendering of elements

--- www/src/pages/secret.tsx
+++ www/src/pages/secret.tsx
@@ -14,11 +14,10 @@ import Blogs from "./components/Blogs";
 import { trpc } from "../utils/trpc";
 
 const secret: NextPage = () => {
-
   const [isAdmin, setIsAdmin] = useState<boolean>(false);
-  const [password, setPassword] = useState<string>("")
+  const [password, setPassword] = useState<string>("");
 
-  const clickHandler = (e:any) => {
+  const clickHandler = () => {
     if (process.env.NEXT_PUBLIC_ADMIN_PASSWORD === password) {
       setIsAdmin(true);
     }
@@ -35,7 +34,7 @@ const secret: NextPage = () => {
           placeholder="password"
           className="block w-50 rounded-md border border-gray-300 px-5 py-3 text-base text-gray-900 placeholder-gray-500 shadow-sm focus:border-rose-500 focus:ring-rose-500 mr-8">
         </input>
-        <button onClick={e => clickHandler(e)} className="blockrounded-md border rounded border-transparent bg-rose-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10">Submit</button>
+        <button onClick={clickHandler} className="blockrounded-md border rounded border-transparent bg-rose-500 px-5 py-3 text-base font-medium text-white shadow hover:bg-rose-600 focus:outline-none focus:ring-2 focus:ring-rose-500 focus:ring-offset-2 sm:px-10">Submit</button>
       </div>}
         {isAdmin && <div>You logged in!</div>}
 
