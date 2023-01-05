f00b6de612911a412f8907592a6588c8054d8ebb
wiltonlee948 <wiltonlee948@gmail.com>
2023-01-04 22:37:15 -0800

    added unit tests to TeamSection component

--- www/__tests__/TeamSection.test.tsx
+++ www/__tests__/TeamSection.test.tsx
@@ -1,11 +1,18 @@
 import {screen, render} from '@testing-library/react';
 import '@testing-library/jest-dom/extend-expect';
-import { Profile } from '../src/pages/components/TeamSection';
+import People, { Profile } from '../src/pages/components/TeamSection';
 
 // http://localhost/_next/image?url=https%3A%2F%2Fgithub.com%2Fwiltonlee948.png&w=256&q=75
 
-test('sets src to /profileFallback.png if src causes an error', async () => {
-  render(<Profile key='' profile='qcvber' name='' />);
-  const src = screen.getByTestId('image').getAttribute('src');
-  console.debug(src)
+it('test to see if all the contributor images show up', async () => {
+  render(<People />);
+  const image = screen.getAllByTestId('image');
+  expect(image.length).toBe(72);
+});
+
+
+test('the value from the profile attribute should be in the src', async () => {
+  render(<Profile key='' profile='react' name='' />);
+  const src = await screen.getByTestId('image').getAttribute('src');
+  expect(src).toContain('react');
 });
