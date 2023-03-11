a79da75dd41747b4e28dced5eaecde7eaceda166
Peter Lam <peterlam@Peters-MBP-2.home>
2023-03-11 08:40:29 -0800

    next demo app added

---
+++ demo-app-next/src/components/Buttons.tsx
@@ -0,0 +1,19 @@
+import Increment from "./Increment";
+
+export default function Buttons() {
+  const buttons = [];
+  for (let i = 0; i < 4; i++) {
+    buttons.push(<Increment />);
+  }
+
+  return (
+    <div className="buttons">
+      <h1>Stateful Buttons</h1>
+      <h4>
+        These buttons are functional components that each manage their own state
+        with the useState hook.
+      </h4>
+      {buttons}
+    </div>
+  );
+}
