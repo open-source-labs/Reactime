6231f2ef1f23b15354da770d556294df0e38fb2b
Jackie Yuan <yuanjackie1@gmail.com>
2023-04-30 20:14:48 -0400

    Fully implemented TS for demo-app & demo-app-next

---
+++ demo-app-next/src/pages/_app.tsx
@@ -0,0 +1,6 @@
+import "../../styles/style.css";
+import React from "react"
+
+export default function MyApp({ Component, pageProps }): JSX.Element {
+  return <Component {...pageProps} />;
+}
--- demo-app/src/client/Components/Nav.tsx
+++ demo-app/src/client/Components/Nav.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-function Nav() {
+function Nav(): JSX.Element {
   return (
     <div className='nav'>
       <Link className='link' to='/'>
