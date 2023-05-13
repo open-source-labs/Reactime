403877edcdb88168e8cf0d72881c8919d1ec3672
Minzo Kim <minzo.kim@gmail.com>
2023-05-13 12:02:27 -0400

    deleted extra testing folder
    Co-authored-by: Mark Teets <markteets@gmail.com>
    Co-authored-by: Jasmine <jasnoo@users.noreply.github.com>
    Co-authored-by: Jackie Yuan <yuanjackie1@users.noreply.github.com>"

--- src/app/__tests_minzo__/SwitchApp.test.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { render, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import SwitchAppDropdown from '../components/SwitchApp';
-
-// describe('Unit testin for SwitchApp.tsx');
