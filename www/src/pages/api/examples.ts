efbe28de0824005baf0e26ba13d681ecd30958a7
Mark Teets <markteets@gmail.com>
2023-05-17 13:53:06 -0600

    Deleted old files
    Co-authored-by: Jasmine Noor <jasnoo@users.noreply.github.com>
    Co-authored-by: Minzo Kim <minzo-kim@users.noreply.github.com>
    Co-authored-by: Jackie Yuan <yuanjackie1@users.noreply.github.com>

--- www/src/pages/api/examples.ts
+++
@@ -1,9 +0,0 @@
-import { type NextApiRequest, type NextApiResponse } from 'next';
-
-import { prisma } from '../../server/db/client';
-
-const examples = async (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: 'John Doe' });
-};
-
-export default examples;
