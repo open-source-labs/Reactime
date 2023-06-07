1f853c70787677fe0e6ee324ae00bc353f0e4dcb
elduke717 <spacematcha@proton.me>
2023-06-07 14:54:22 -0400

    add some tests

--- src/backend/routers/snapShot.ts
+++ src/backend/routers/snapShot.ts
@@ -2,6 +2,7 @@ import { Snapshot, FiberRoot } from '../types/backendTypes';
 import componentActionsRecord from '../models/masterState';
 import routes from '../models/routes';
 import createTree from '../controllers/createTree';
+const _ = require('lodash');
 
 // -------------------------UPDATE & SEND TREE SNAP SHOT------------------------
 /**
@@ -13,6 +14,42 @@ import createTree from '../controllers/createTree';
  */
 // updating tree depending on current mode on the panel (pause, etc)
 export default function updateAndSendSnapShotTree(fiberRoot: FiberRoot): void {
+  
+  // This function compares trees with one another
+  function isIdentical(tree1, tree2) {
+    // Check if both are null
+    if (!tree1 && !tree2) return true;
+  
+    // Check if either of them is null
+    if (!tree1 || !tree2) return false;
+  
+    // Get the keys
+    const tree1Keys = Object.keys(tree1);
+    const tree2Keys = Object.keys(tree2);
+  
+    // Check if length of keys is different
+    if (tree1Keys.length !== tree2Keys.length) return false;
+  
+    // Check keys and values
+    for (let key of tree1Keys) {
+      const val1 = tree1[key];
+      const val2 = tree2[key];
+  
+      const areObjects = isObject(val1) && isObject(val2);
+      if (
+        (areObjects && !isIdentical(val1, val2)) ||
+        (!areObjects && val1 !== val2)
+      ) {
+        return false;
+      }
+    }
+    return true;
+  }
+  
+  function isObject(object) {
+    return object != null && typeof object === 'object';
+  }
+  
   // This is the currently active root fiber(the mutable root of the tree)
   const { current } = fiberRoot;
   // Clear all of the legacy actions from old fiber tree because we are about to create a new one
@@ -29,7 +66,16 @@ export default function updateAndSendSnapShotTree(fiberRoot: FiberRoot): void {
   // the postMessage action will be received on the content script to later update the tabsObj
   // this will fire off everytime there is a change in test application
   // convert the payload from a fiber tree to an object to avoid a data clone error when postMessage processes the argument
+  console.log('This is the payload, baby: ', payload);
+  const clonedDeepPayload = _.cloneDeep(payload);
+  console.log('cloneDeepPayload tree', clonedDeepPayload);
+  // compare payload and clonedDeepPayload with isIdentical
+  // console.log('are they identical?', isIdentical(payload, clonedDeepPayload));
+  // console.log('typeof payload', typeof payload);
   const obj = JSON.parse(JSON.stringify(payload));
+  // console.log('righton, this is the obj: ', obj)
+  // console.log('typeof obj', typeof obj);
+  console.log('passing in obj');
   window.postMessage(
     {
       action: 'recordSnap',
