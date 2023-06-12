b02dc1b2c6dac3aed5538a72da077ca300047d77
j-mccoll <100531603+j-mccoll@users.noreply.github.com>
2023-06-12 18:59:17 -0400

    Update StateRoute.tsx

--- src/app/components/StateRoute/StateRoute.tsx
+++ src/app/components/StateRoute/StateRoute.tsx
@@ -22,7 +22,7 @@ const NO_STATE_MSG = 'No state change detected. Trigger an event to change state
 
 
 const StateRoute = (props: StateRouteProps) => {
-  const { snapshot, hierarchy, snapshots, viewIndex, webMetrics, currLocation } = props; 
+  const { snapshot, hierarchy, snapshots, viewIndex, webMetrics, currLocation } = props;
   const [{ tabs, currentTab }, dispatch] = useStoreContext();
   const { hierarchy, sliderIndex, viewIndex } = tabs[currentTab];
 
