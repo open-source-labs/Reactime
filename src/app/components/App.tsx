98618f0d5ac2717774e5798deceec8e6c9c587fb
Christopher Stamper <ctstamper@Christophers-MacBook-Air.local>
2023-10-09 18:58:24 -0700

    Fixed merge conflict

--- src/app/components/App.tsx
+++ src/app/components/App.tsx
@@ -10,32 +10,12 @@ import theme from './theme';
   This is used to determine the proper tutorial to render when How To button is pressed.
 */
 
-<<<<<<< HEAD
-// const initialState: InitialStateProps = { // we initialize what our initialState is here
-//   port: null,
-//   currentTab: null,
-//   currentTitle: 'No Target',
-//   tabs: {},
-//   currentTabInApp: null,
-//   connectionStatus: true,
-//   reconnectRequested: false,
-// };
-
-=======
->>>>>>> dev
 function App(): JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <Router>
         {/* we wrap our application with the <Router> tag so that all components that are nested will have the react-router context */}
-<<<<<<< HEAD
-        {/* <StoreContext.Provider value={useReducer(mainReducer, initialState)}> */}
-          {/* we wrap our MainContainer with the provider so that we will be able to use the store context. We create our store by using useReducer and passing it into the value property */}
           <MainContainer />
-        {/* </StoreContext.Provider> */}
-=======
-          <MainContainer />
->>>>>>> dev
       </Router>
     </ThemeProvider>
   );
