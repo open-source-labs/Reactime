b66cf9ff64c88e67218abffdfd49517762f72cd5
kelvinmirhan <kelvinmirhan@gmail.com>
2023-10-16 21:29:00 -0700

    got dropdown.test.tsx to pass

--- src/app/__tests__/dropdown.test.tsx
+++ src/app/__tests__/dropdown.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
+import { render, fireEvent } from '@testing-library/react';
 import Dropdown from '../components/Dropdown';
+import '@testing-library/jest-dom/extend-expect';
 
-describe('Dropdown test', () => {
+describe('Dropdown component test', () => {
   const speeds = [
     { value: 1, label: 'test1' },
     { value: 2, label: 'test2' },
@@ -11,11 +11,32 @@ describe('Dropdown test', () => {
   ];
   const setSpeed = jest.fn();
 
-  test('renders Dropdown with correct info', () => {
+  test('renders Dropdown initial selection', () => {
     const currSpeed = speeds[0];
-    const { container } = render(
+    const renderResult = render(
       <Dropdown speeds={speeds} setSpeed={setSpeed} selectedSpeed={currSpeed} />,
     );
-    expect(container.querySelector('.react-select__single-value')).toHaveTextContent('test1');
+
+    // Check if the initial selected speed is rendered
+    expect(renderResult.getByText('test1')).toBeInTheDocument();
+  });
+
+  test('handles dropdown interaction and option selection', () => {
+    const renderResult = render(
+      <Dropdown speeds={speeds} setSpeed={setSpeed} selectedSpeed={speeds[0]} />
+    );
+  
+    // Find the dropdown trigger element
+    const dropdownTrigger = renderResult.getByText('test1');
+  
+    // Simulate opening the dropdown
+    fireEvent.mouseDown(dropdownTrigger);
+  
+    // Simulate selecting an option
+    fireEvent.click(renderResult.getByText('test2'));
+  
+    // Ensure that setSpeed was called
+    expect(setSpeed).toHaveBeenCalled();
   });
 });
+
