1368caa728aac9ca2138e6a3611446e3ef965b62
kelvinmirhan <kelvinmirhan@gmail.com>
2023-10-18 03:27:50 -0700

    removed old travelContainer test file, commented out old actioncontainer test file

--- src/app/__tests__/ActionContainer.test.tsx
+++ src/app/__tests__/ActionContainer.test.tsx
@@ -1,180 +1,180 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
-/* eslint-disable react/jsx-filename-extension */
-import React from 'react';
-import { render as rtlRender, screen, fireEvent } from '@testing-library/react';
-import '@testing-library/jest-dom/extend-expect';
-import ActionContainer from '../containers/ActionContainer';
-import { useStoreContext } from '../store';
-import TravelContainer from '../containers/TravelContainer';
-import { Provider, useDispatch, useSelector } from 'react-redux';
-import { store } from '../RTKstore';
-//so far i have imported provider, usedispatch, useselector, and store 
-//wrapped components in provider
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// /* eslint-disable react/jsx-filename-extension */
+// import React from 'react';
+// import { render as rtlRender, screen, fireEvent } from '@testing-library/react';
+// import '@testing-library/jest-dom/extend-expect';
+// import ActionContainer from '../containers/ActionContainer';
+// import { useStoreContext } from '../store';
+// import TravelContainer from '../containers/TravelContainer';
+// import { Provider, useDispatch, useSelector } from 'react-redux';
+// import { store } from '../RTKstore';
+// //so far i have imported provider, usedispatch, useselector, and store 
+// //wrapped components in provider
 
-// jest.mock('react-redux', () => ({
-//   ...jest.requireActual('react-redux'), // include all the exports from the actual react-redux module
-//   useDispatch: jest.fn(),
-//   useSelector: jest.fn() //override the useDispatch from the react redux module with a jest mock function
-// }));
+// // jest.mock('react-redux', () => ({
+// //   ...jest.requireActual('react-redux'), // include all the exports from the actual react-redux module
+// //   useDispatch: jest.fn(),
+// //   useSelector: jest.fn() //override the useDispatch from the react redux module with a jest mock function
+// // }));
 
-// const render = component => rtlRender(
-//   <Provider store={store}>
-//     {component}
-//   </Provider>
-// )
-
-
-const state = {
-  tabs: {
-    87: {
-      snapshots: [1, 2, 3, 4],
-      hierarchy: {
-        index: 0,
-        name: 1,
-        branch: 0,
-        stateSnapshot: {
-          state: {},
-          children: [
-            {
-              state: { test: 'test' },
-              name: 'App',
-              componentData: { actualDuration: 3.5 },
-            },
-          ],
-          route: {
-            id: 1,
-            url: 'http://localhost:8080/',
-          },
-        },
-        children: [
-          {
-            index: 1,
-            name: 2,
-            branch: 0,
-            stateSnapshot: {
-              state: {},
-              children: [
-                {
-                  state: { test: 'test' },
-                  name: 'App',
-                  componentData: { actualDuration: 3.5 },
-                },
-              ],
-              route: {
-                id: 2,
-                url: 'http://localhost:8080/',
-              },
-            },
-            children: [
-              {
-                index: 2,
-                name: 3,
-                branch: 0,
-                stateSnapshot: {
-                  state: {},
-                  children: [
-                    {
-                      state: { test: 'test' },
-                      name: 'App',
-                      componentData: { actualDuration: 3.5 },
-                    },
-                  ],
-                  route: {
-                    id: 3,
-                    url: 'http://localhost:8080/',
-                  },
-                },
-                children: [
-                  {
-                    index: 3,
-                    name: 4,
-                    branch: 0,
-                    stateSnapshot: {
-                      state: {},
-                      children: [
-                        {
-                          state: { test: 'test' },
-                          name: 'App',
-                          componentData: { actualDuration: 3.5 },
-                        },
-                      ],
-                      route: {
-                        id: 4,
-                        url: 'http://localhost:8080/test/',
-                      },
-                    },
-                    children: [],
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      },
-      currLocation: {
-        index: 0,
-        name: 1,
-        branch: 0,
-      },
-      sliderIndex: 0,
-      viewIndex: -1,
-    },
-  },
-  currentTab: 87,
-};
-//creates jest mock function to simulate behavior of functions/methods
-const dispatch = jest.fn();
-
-jest.spyOn(React, 'useEffect').mockImplementation(() => jest.fn());
-//This line spies on the useEffect function from React, replacing it with a mocked implementation that returns an empty Jest mock function, 
-//effectively disabling its actual side effects during testing.
-jest.mock('../store');
-//This line mocks the import of a module located at '../store', which can be useful to isolate components from real Redux store behavior 
-//and provide custom mock behavior for testing purposes.
-
-const mockeduseStoreContext = jest.mocked(useStoreContext);
-mockeduseStoreContext.mockImplementation(() => [state, dispatch]); //After creating the mock, this line configures the mock to implement a specific behavior. In this case, it specifies that when useStoreContext is called, it should return an array containing two values: state and dispatch.
+// // const render = component => rtlRender(
+// //   <Provider store={store}>
+// //     {component}
+// //   </Provider>
+// // )
 
 
-////////////////////////////////////////////////////////////////////////////////////
-const MockRouteDescription = jest.fn();
-jest.mock('../components/RouteDescription', () => () => {
-  MockRouteDescription();
-  return <div>MockRouteDescription</div>;
-});
+// const state = {
+//   tabs: {
+//     87: {
+//       snapshots: [1, 2, 3, 4],
+//       hierarchy: {
+//         index: 0,
+//         name: 1,
+//         branch: 0,
+//         stateSnapshot: {
+//           state: {},
+//           children: [
+//             {
+//               state: { test: 'test' },
+//               name: 'App',
+//               componentData: { actualDuration: 3.5 },
+//             },
+//           ],
+//           route: {
+//             id: 1,
+//             url: 'http://localhost:8080/',
+//           },
+//         },
+//         children: [
+//           {
+//             index: 1,
+//             name: 2,
+//             branch: 0,
+//             stateSnapshot: {
+//               state: {},
+//               children: [
+//                 {
+//                   state: { test: 'test' },
+//                   name: 'App',
+//                   componentData: { actualDuration: 3.5 },
+//                 },
+//               ],
+//               route: {
+//                 id: 2,
+//                 url: 'http://localhost:8080/',
+//               },
+//             },
+//             children: [
+//               {
+//                 index: 2,
+//                 name: 3,
+//                 branch: 0,
+//                 stateSnapshot: {
+//                   state: {},
+//                   children: [
+//                     {
+//                       state: { test: 'test' },
+//                       name: 'App',
+//                       componentData: { actualDuration: 3.5 },
+//                     },
+//                   ],
+//                   route: {
+//                     id: 3,
+//                     url: 'http://localhost:8080/',
+//                   },
+//                 },
+//                 children: [
+//                   {
+//                     index: 3,
+//                     name: 4,
+//                     branch: 0,
+//                     stateSnapshot: {
+//                       state: {},
+//                       children: [
+//                         {
+//                           state: { test: 'test' },
+//                           name: 'App',
+//                           componentData: { actualDuration: 3.5 },
+//                         },
+//                       ],
+//                       route: {
+//                         id: 4,
+//                         url: 'http://localhost:8080/test/',
+//                       },
+//                     },
+//                     children: [],
+//                   },
+//                 ],
+//               },
+//             ],
+//           },
+//         ],
+//       },
+//       currLocation: {
+//         index: 0,
+//         name: 1,
+//         branch: 0,
+//       },
+//       sliderIndex: 0,
+//       viewIndex: -1,
+//     },
+//   },
+//   currentTab: 87,
+// };
+// //creates jest mock function to simulate behavior of functions/methods
+// const dispatch = jest.fn();
 
-const MockSwitchApp = jest.fn();
-jest.mock('../components/SwitchApp', () => () => {
-  MockSwitchApp();
-  return <div>MockSwitchApp</div>;
-});
+// jest.spyOn(React, 'useEffect').mockImplementation(() => jest.fn());
+// //This line spies on the useEffect function from React, replacing it with a mocked implementation that returns an empty Jest mock function, 
+// //effectively disabling its actual side effects during testing.
+// jest.mock('../store');
+// //This line mocks the import of a module located at '../store', which can be useful to isolate components from real Redux store behavior 
+// //and provide custom mock behavior for testing purposes.
 
-describe('unit testing for ActionContainer', () => {
-  beforeEach(() => {
-    mockeduseStoreContext.mockClear();
-    dispatch.mockClear();
-    render(
-      <ActionContainer actionView={true} />
-    )
-  });
+// const mockeduseStoreContext = jest.mocked(useStoreContext);
+// mockeduseStoreContext.mockImplementation(() => [state, dispatch]); //After creating the mock, this line configures the mock to implement a specific behavior. In this case, it specifies that when useStoreContext is called, it should return an array containing two values: state and dispatch.
 
-  test('Expect top arrow to be rendered', () => {
-    expect(screen.getByRole('complementary')).toBeInTheDocument();
-  });
 
-  test('Expect RouteDescription to be rendered', () => {
-    expect(screen.getAllByText('MockRouteDescription')).toHaveLength(2);
-  });
+// ////////////////////////////////////////////////////////////////////////////////////
+// const MockRouteDescription = jest.fn();
+// jest.mock('../components/RouteDescription', () => () => {
+//   MockRouteDescription();
+//   return <div>MockRouteDescription</div>;
+// });
 
-  test('Expect SwitchApp to be rendered', () => {
-    expect(screen.getByText('MockSwitchApp')).toBeInTheDocument();
-  });
+// const MockSwitchApp = jest.fn();
+// jest.mock('../components/SwitchApp', () => () => {
+//   MockSwitchApp();
+//   return <div>MockSwitchApp</div>;
+// });
 
-  test('Click works on clear button', () => {
-    fireEvent.click(screen.getAllByRole('button')[0]);
-    expect(dispatch).toHaveBeenCalledTimes(1);
-  });
-});
+// describe('unit testing for ActionContainer', () => {
+//   beforeEach(() => {
+//     mockeduseStoreContext.mockClear();
+//     dispatch.mockClear();
+//     render(
+//       <ActionContainer actionView={true} />
+//     )
+//   });
+
+//   test('Expect top arrow to be rendered', () => {
+//     expect(screen.getByRole('complementary')).toBeInTheDocument();
+//   });
+
+//   test('Expect RouteDescription to be rendered', () => {
+//     expect(screen.getAllByText('MockRouteDescription')).toHaveLength(2);
+//   });
+
+//   test('Expect SwitchApp to be rendered', () => {
+//     expect(screen.getByText('MockSwitchApp')).toBeInTheDocument();
+//   });
+
+//   test('Click works on clear button', () => {
+//     fireEvent.click(screen.getAllByRole('button')[0]);
+//     expect(dispatch).toHaveBeenCalledTimes(1);
+//   });
+// });
 
 // describe('integration testing for ActionContainer', () => {
   // beforeEach(() => {
@@ -188,8 +188,8 @@ describe('unit testing for ActionContainer', () => {
   //   )
   // });
 
-  test('Slider resets on clear button', () => {
-    fireEvent.click(screen.getAllByRole('button')[0]);
-    expect(screen.getByRole('slider')).toHaveStyle('left: 0');
-  });
-});
+//   test('Slider resets on clear button', () => {
+//     fireEvent.click(screen.getAllByRole('button')[0]);
+//     expect(screen.getByRole('slider')).toHaveStyle('left: 0');
+//   });
+// });
