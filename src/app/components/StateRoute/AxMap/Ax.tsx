f8445f5197bbf299ab4881b1c2e65aec980265e8
ay7991 <amy.yang7991@gmail.com>
2024-02-10 14:50:54 -0500

    testing visx visualization with example data

---
+++ src/app/components/StateRoute/AxMap/Ax.tsx
@@ -0,0 +1,234 @@
+import React, { useState } from 'react';
+import { Group } from '@visx/group';
+import { hierarchy, Tree } from '@visx/hierarchy';
+import { LinearGradient } from '@visx/gradient';
+import { pointRadial } from 'd3-shape';
+import useForceUpdate from './useForceUpdate';
+import LinkControls from './axLinkControls';
+import getLinkComponent from './getAxLinkComponents';
+
+const theme = {
+  scheme: 'monokai',
+  author: 'wimer hazenberg (http://www.monokai.nl)',
+  base00: '#272822',
+  base01: '#383830',
+  base02: '#49483e',
+  base03: '#75715e',
+  base04: '#a59f85',
+  base05: '#f8f8f2',
+  base06: '#f5f4f1',
+  base07: '#f9f8f5',
+  base08: '#f92672',
+  base09: '#fd971f',
+  base0A: '#f4bf75',
+  base0B: '#a6e22e',
+  base0C: '#a1efe4',
+  base0D: '#66d9ef',
+  base0E: '#ae81ff',
+  base0F: '#cc6633',
+};
+
+interface TreeNode {
+  name: string;
+  isExpanded?: boolean;
+  children?: TreeNode[];
+}
+
+const data: TreeNode = {
+  name: 'T',
+  children: [
+    {
+      name: 'A',
+      children: [
+        { name: 'A1' },
+        { name: 'A2' },
+        { name: 'A3' },
+        {
+          name: 'C',
+          children: [
+            {
+              name: 'C1',
+            },
+            {
+              name: 'D',
+              children: [
+                {
+                  name: 'D1',
+                },
+                {
+                  name: 'D2',
+                },
+                {
+                  name: 'D3',
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    },
+    { name: 'Z' },
+    {
+      name: 'B',
+      children: [{ name: 'B1' }, { name: 'B2' }, { name: 'B3' }],
+    },
+  ],
+};
+
+const defaultMargin = { 
+  top: 30,
+  left: 30,
+  right: 55,
+  bottom: 70, 
+};
+
+export type LinkTypesProps = {
+  width: number;
+  height: number;
+  margin?: { top: number; right: number; bottom: number; left: number };
+};
+
+export default function AxTree({
+  width: totalWidth,
+  height: totalHeight,
+  margin = defaultMargin,
+}: LinkTypesProps) {
+  const [layout, setLayout] = useState('cartesian');
+  const [orientation, setOrientation] = useState('horizontal');
+  const [linkType, setLinkType] = useState('diagonal');
+  const [stepPercent, setStepPercent] = useState(0.5);
+  const forceUpdate = useForceUpdate();
+
+  const innerWidth: number = totalWidth - margin.left - margin.right;
+  const innerHeight: number = totalHeight - margin.top - margin.bottom - 60;
+
+  let origin: { x: number; y: number };
+  let sizeWidth: number;
+  let sizeHeight: number;
+
+  if (layout === 'polar') {
+    origin = {
+      x: innerWidth / 2,
+      y: innerHeight / 2,
+    };
+    sizeWidth = 2 * Math.PI;
+    sizeHeight = Math.min(innerWidth, innerHeight) / 2;
+  } else {
+    origin = { x: 0, y: 0 };
+    if (orientation === 'vertical') {
+      sizeWidth = innerWidth;
+      sizeHeight = innerHeight;
+    } else {
+      sizeWidth = innerHeight;
+      sizeHeight = innerWidth;
+    }
+  }
+
+  const LinkComponent = getLinkComponent({ layout, linkType, orientation });
+
+  return totalWidth < 10 ? null : (
+    <div>
+      <LinkControls
+        layout={layout}
+        orientation={orientation}
+        linkType={linkType}
+        stepPercent={stepPercent}
+        setLayout={setLayout}
+        setOrientation={setOrientation}
+        setLinkType={setLinkType}
+        setStepPercent={setStepPercent}
+      />
+      <svg width={totalWidth} height={totalHeight}>
+        <LinearGradient id="links-gradient" from="#fd9b93" to="#fe6e9e" />
+        <rect width={totalWidth} height={totalHeight} rx={14} fill="#272b4d" />
+        <Group top={margin.top} left={margin.left}>
+          <Tree
+            root={hierarchy(data, (d) => (d.isExpanded ? null : d.children))}
+            size={[sizeWidth, sizeHeight]}
+            separation={(a, b) => (a.parent === b.parent ? 1 : 0.5) / a.depth}
+          >
+            {(tree) => (
+              <Group top={origin.y} left={origin.x}>
+                {tree.links().map((link, i) => (
+                  <LinkComponent
+                    key={i}
+                    data={link}
+                    percent={stepPercent}
+                    stroke="rgb(254,110,158,0.6)"
+                    strokeWidth="1"
+                    fill="none"
+                  />
+                ))}
+
+                {tree.descendants().map((node, key) => {
+                  const width = 40;
+                  const height = 20;
+
+                  let top: number;
+                  let left: number;
+                  if (layout === 'polar') {
+                    const [radialX, radialY] = pointRadial(node.x, node.y);
+                    top = radialY;
+                    left = radialX;
+                  } else if (orientation === 'vertical') {
+                    top = node.y;
+                    left = node.x;
+                  } else {
+                    top = node.x;
+                    left = node.y;
+                  }
+
+                  return (
+                    <Group top={top} left={left} key={key}>
+                      {node.depth === 0 && (
+                        <circle
+                          r={12}
+                          fill="url('#links-gradient')"
+                          onClick={() => {
+                            node.data.isExpanded = !node.data.isExpanded;
+                            console.log(node);
+                            forceUpdate();
+                          }}
+                        />
+                      )}
+                      {node.depth !== 0 && (
+                        <rect
+                          height={height}
+                          width={width}
+                          y={-height / 2}
+                          x={-width / 2}
+                          fill="#272b4d"
+                          stroke={node.data.children ? '#03c0dc' : '#26deb0'}
+                          strokeWidth={1}
+                          strokeDasharray={node.data.children ? '0' : '2,2'}
+                          strokeOpacity={node.data.children ? 1 : 0.6}
+                          rx={node.data.children ? 0 : 10}
+                          onClick={() => {
+                            node.data.isExpanded = !node.data.isExpanded;
+                            console.log(node);
+                            forceUpdate();
+                          }}
+                        />
+                      )}
+                      <text
+                        dy=".33em"
+                        fontSize={9}
+                        fontFamily="Arial"
+                        textAnchor="middle"
+                        style={{ pointerEvents: 'none' }}
+                        fill={node.depth === 0 ? '#71248e' : node.children ? 'white' : '#26deb0'}
+                      >
+                        {node.data.name}
+                      </text>
+                    </Group>
+                  );
+                })}
+              </Group>
+            )}
+          </Tree>
+        </Group>
+      </svg>
+    </div>
+  );
+}
+
--- src/app/components/StateRoute/StateRoute.tsx
+++ src/app/components/StateRoute/StateRoute.tsx
@@ -15,7 +15,7 @@ import { useSelector } from 'react-redux';
 import PerformanceVisx from './PerformanceVisx/PerformanceVisx';
 import WebMetricsContainer from './WebMetrics/WebMetricsContainer';
 import { MainState, RootState, StateRouteProps } from '../../FrontendTypes';
-import AxTree from './Ax';
+import AxTree from './AxMap/Ax';
 
 /*
   Loads the appropriate StateRoute view and renders the Map, Performance, History, Webmetrics, and Tree navbar buttons after clicking on the 'State' button located near the top rightmost corner.
