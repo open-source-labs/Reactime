88c45645d9dea8c2c818bb3d7116670e32da9a53
ay7991 <amy.yang7991@gmail.com>
2024-02-14 16:18:03 -0500

    added axLegendSlice reducer and exported RootState type

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,11 +1,15 @@
 //Import store from redux tool kit
 import { configureStore } from '@reduxjs/toolkit';
 import { mainSlice } from './slices/mainSlice';
+import { axLegendSlice } from './slices/AxSlices/axLegendSlice';
 
 //Export Store
 export const store = configureStore({
   reducer: {
     main: mainSlice.reducer,
+    axLegend: axLegendSlice.reducer,
   },
   middleware: (getDefaultMiddleware) => getDefaultMiddleware({ serializableCheck: false }),
 });
+
+export type RootState = ReturnType<typeof store.getState>
