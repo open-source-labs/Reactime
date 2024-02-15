f5c3f2b0b73980de1c4cfd9c4c273b69ed36bcd3
ay7991 <amy.yang7991@gmail.com>
2024-02-14 20:19:20 -0500

    created axLegend component, updated information being rendered

--- src/app/components/StateRoute/AxMap/axLegend.tsx
+++ src/app/components/StateRoute/AxMap/axLegend.tsx
@@ -3,7 +3,19 @@ import React from 'react';
 const AxLegend = () => {
     return (
         <div>
-            testing
+            Nodes from the accessibility tree have either a role <strong>role</strong> or <strong>internalRole</strong>
+            <ul>
+                <li>
+                    Role refers to <strong> ARIA </strong> roles, which indicate the purpose of the element to assistive technologies, like screen readers
+                </li>
+                <li>
+                    internalRole refers to browser-specific roles <strong> Chrome </strong> for its accessibility processing
+                </li>
+            </ul>
+
+            <div className='axLegendInfo' id='roleNodes'>
+                <h4>All of the nodes here have the role 'role' </h4>
+            </div>
         </div>
     );
 }
