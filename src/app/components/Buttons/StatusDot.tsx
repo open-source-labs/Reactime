63573cb2043fba5471c60317470c9209187402a8
Garrett <garrettlchow@gmail.com>
2024-12-10 13:49:45 -0800

    styled reconnect green dot

--- src/app/components/Buttons/StatusDot.tsx
+++ src/app/components/Buttons/StatusDot.tsx
@@ -1,26 +1,7 @@
 import React from 'react';
-import styled from 'styled-components';
-
-const Dot = styled('div')({
-  borderRadius: '50%',
-  width: '12px',
-  height: '12px',
-  display: 'inline-block',
-});
 
 const StatusDot = ({ status }) => {
-  let color: 'green' | 'red';
-  switch (status) {
-    case 'active':
-      color = 'green';
-      break;
-    case 'inactive':
-      color = 'red';
-      break;
-    default:
-      color = 'green';
-  }
-  return <Dot style={{ backgroundColor: color }} />;
+  return <span className={`status-dot ${status}`} />;
 };
 
 export default StatusDot;
