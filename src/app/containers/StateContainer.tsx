a8e59dd52207571c394cf3ff6451e9752f07244b
Garrett <garrettlchow@gmail.com>
2024-12-11 09:00:40 -0800

    removed state and diff buttons

--- src/app/containers/StateContainer.tsx
+++ src/app/containers/StateContainer.tsx
@@ -18,47 +18,19 @@ const StateContainer = (props: StateContainerProps): JSX.Element => {
     viewIndex, // from 'tabs[currentTab]' object in 'MainContainer'
     webMetrics, // from 'tabs[currentTab]' object in 'MainContainer'
     currLocation, // from 'tabs[currentTab]' object in 'MainContainer'
-    axSnapshots,// from 'tabs[currentTab]' object in 'MainContainer'
+    axSnapshots, // from 'tabs[currentTab]' object in 'MainContainer'
   } = props;
 
   return (
     <>
-    <div className='state-container'>
-      <div className='main-navbar-container'>
-        <div className='main-navbar-text' />
-        <div className='main-navbar'>
-          <NavLink
-            className={(navData) =>
-              navData.isActive ? 'is-active main-router-link' : 'main-router-link'
-            }
-            to='/'
-          >
-            State
-          </NavLink>
-          <NavLink
-            className={(navData) =>
-              navData.isActive ? 'is-active main-router-link' : 'main-router-link'
-            }
-            to='/diff'
-          >
-            Diff
-          </NavLink>
+      <div className='state-container'>
+        <div className='main-navbar-container'>
+          <div className='main-navbar'></div>
         </div>
-      </div>
-      <Routes>
-        <Route
-          path='/diff/*'
-          element={
-            <div>
-              <DiffRoute snapshot={snapshot} />
-              {/* <Outlet/> */}
-            </div>
-          }
-        />
-        <Route
-          path='/*'
-          element={
-            <div style={{ height: '100%' }}>
+        <Routes>
+          <Route
+            path='/*'
+            element={
               <StateRoute
                 axSnapshots={axSnapshots}
                 webMetrics={webMetrics}
@@ -68,11 +40,10 @@ const StateContainer = (props: StateContainerProps): JSX.Element => {
                 snapshots={snapshots}
                 currLocation={currLocation}
               />
-            </div>
-          }
-        />
-      </Routes>
-    </div>
+            }
+          />
+        </Routes>
+      </div>
     </>
   );
 };
