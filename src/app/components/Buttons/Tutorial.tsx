08c39d52bac02c79b18b090283a33a98d63a1ad7
dryczek01 <dryczek14@gmail.com>
2024-12-21 12:20:01 -0500

    Tutorial needs updated dev code

--- src/app/components/Buttons/Tutorial.tsx
+++ src/app/components/Buttons/Tutorial.tsx
@@ -185,8 +185,7 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
           {
             title: 'Accessibility Tree',
             element: '.accessibility-tab',
-            intro:
-              '<ul><li>This tab visually displays a Accessibility Tree</li></ul>',
+            intro: '<ul><li>This tab visually displays a Accessibility Tree</li></ul>',
             position: 'bottom',
           },
           {
@@ -206,45 +205,35 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
               '<ul><li>Here we can analyze the render times of our app</li> <li>This is the current series of state changes within our app</li> <li>Mouse over the bargraph elements for details on each specific component</li></ul>',
             position: 'top',
           },
-          {
-            title: 'Saving Series & Actions',
-            element: '.save-series-button',
-            intro: '<ul><li>Click here to save your current series data</li></ul>',
-            position: 'top',
-          },
-          {
-            title: 'Saving Series & Actions',
-            element: '#seriesname',
-            intro: '<ul><li>We can now give our series a name or leave it at the default</li></ul>',
-            position: 'top',
-          },
-          {
-            title: 'Saving Series & Actions',
-            element: '.actionname',
-            intro:
-              '<ul><li>If we wish to save a specific action to compare later, give it a name here</li></ul>',
-            position: 'top',
-          },
-          {
-            title: 'Saving Series & Actions',
-            element: '.save-series-button',
-            intro:
-              '<ul><li>Press save series again.</li> <li>Your series and actions are now saved!</li></ul>',
-            position: 'top',
-          },
-          {
-            title: 'Comparison Tab',
-            element: '#router-link-performance-comparison',
-            intro: "<ul><li>Now let's head over to the comparison tab</li></ul>",
-            position: 'top',
-          },
-          {
-            title: 'Comparing Series',
-            intro: '<ul><li>Here we can select a saved series or action to compare</li></ul>',
-            position: 'top',
-          },
         ];
         break;
+        //       <div>
+        //     Nodes from the accessibility tree have either a role <strong>role</strong> or <strong>internalRole</strong>
+        //     <ul>
+        //       <li>
+        //         <i><b>Role</b></i> refers to <strong> ARIA </strong> roles, which indicate the purpose of the element to assistive technologies, like screen readers.
+        //         All of the nodes rendered in this tree have a role of 'role'
+        //       </li>
+        //       <li>
+        //         <i><b>internalRole</b></i> refers to browser-specific roles <strong> Chrome </strong> for its own accessibility processing
+        //       </li>
+        //     </ul>
+        //     <p> Each node is given a property labeled <strong>ignored</strong>. Nodes read by the screen reader have their ignored property evaluate to <strong>false</strong>.
+        //     Nodes not read by the screen reader evaluate to <strong>true</strong>.</p>
+        //     <p> Nodes labeled as <strong>no name</strong> are visible to a screen reader, but were not given a name label.</p>
+        //   </div>
+        // );
+        case 'accessibility':
+          steps = [
+            {
+              title: 'Accessibility Tree',
+              element: '.display',
+              intro:
+                '<ul><li>Nodes from the accessibility tree have either a role or internalRole</li></ul>',
+              position: 'top',
+            },
+          ];
+          break;
       case 'webmetrics':
         steps = [
           {
