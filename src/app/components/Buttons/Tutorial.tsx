b62af49eb9f8b4b379214bab59e9aded19d7966e
Garrett <garrettlchow@gmail.com>
2025-01-02 20:40:24 -0800

    added dark theme toggle and reconnect button to tutorial

--- src/app/components/Buttons/Tutorial.tsx
+++ src/app/components/Buttons/Tutorial.tsx
@@ -103,10 +103,10 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
             position: 'right',
           },
           {
-            title: 'Toggle Record Button',
+            title: 'Toggles',
             element: '.record-button-container',
             intro:
-              '<ul><li>Toggle record button to pause state changes on target application</li></ul>',
+              '<ul><li>Toggle record button to pause state changes on target application</li><li>Toggle theme button to switch between light and dark themes</li></ul>',
             position: 'right',
           },
           {
@@ -126,7 +126,14 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
             title: 'Timejump',
             element: '.rc-slider',
             intro:
-              '<ul><li>Use the slider to go back in time to a particular state change</li><li>Click the Play button to run through each state change automatically</li></ul>',
+              '<ul><li>Use the slider to go back in time to a particular state change</li></ul>',
+            position: 'top',
+          },
+          {
+            title: 'Play',
+            element: '.travel-container',
+            intro:
+              '<ul><li>Click the Play button to run through each state change automatically</li><li>Select playback speed from the dropdown menu (0.5x, 1.0x, or 2.0x) to control how fast states change during playback</li></ul>',
             position: 'top',
           },
           {
@@ -150,10 +157,23 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
             position: 'top',
           },
           {
+            title: 'Reconnect button',
+            element: '.reconnect-button',
+            intro:
+              '<ul><li>Click the Reconnect button if connection is lost to reestablish communication with your application.</ul>',
+            position: 'top',
+          },
+          {
             element: '.map-tab',
             title: 'Map Tab',
             intro:
               '<ul><li>This tab visually displays a component hierarchy tree for your app</li></ul>',
+            position: 'bottom',
+          },
+          {
+            title: 'History Tab',
+            element: '.history-tab',
+            intro: '<ul><li>This tab visually displays a history of each snapshot</li></ul>',
             position: 'bottom',
           },
           {
@@ -163,12 +183,7 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
               '<ul><li>User can save a series of state snapshots and use it to analyze changes in component, render performance between current, and previous series of snapshots.</li> <li>User can save a series of state snapshots and use it to analyze changes in component render performance between current and previous series of snapshots.</li> <li>TIP: Click the how to use button within the performance tab for more details.</li> </ul>',
             position: 'bottom',
           },
-          {
-            title: 'History Tab',
-            element: '.history-tab',
-            intro: '<ul><li>This tab visually displays a history of each snapshot</li></ul>',
-            position: 'bottom',
-          },
+
           {
             title: 'Web Metrics Tab',
             element: '.web-metrics-tab',
@@ -185,8 +200,9 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
           },
           {
             title: 'Accessibility Tree',
-            element: '.accessibility-tab',//'<ul><li>This tab visually displays a Accessibility Tree</li></ul>'
-            intro:'<ul><li>Nodes from the accessibility tree have either a role or a internal role refers to ARIA roles, which indicate the purpose of the element to assistive technologies, like screen readers.All of the nodes rendered in this tree have a role of Role.InternalRole refers to browser-specific roles <strong> Chrome </strong> for its own accessibility processing.<p> Each node is given a property labeled <strong>ignored</strong>. Nodes read by the screen reader have their ignored property evaluate to <strong>false</strong>.Nodes not read by the screen reader evaluate to <strong>true</strong>.</p><p> Nodes labeled as <strong>no name</strong> are visible to a screen reader, but were not given a name label.</p></li></ul>',
+            element: '.accessibility-tab', //'<ul><li>This tab visually displays a Accessibility Tree</li></ul>'
+            intro:
+              '<ul><li>Nodes from the accessibility tree have either a role or a internal role refers to ARIA roles, which indicate the purpose of the element to assistive technologies, like screen readers.All of the nodes rendered in this tree have a role of Role.InternalRole refers to browser-specific roles <strong> Chrome </strong> for its own accessibility processing.<p> Each node is given a property labeled <strong>ignored</strong>. Nodes read by the screen reader have their ignored property evaluate to <strong>false</strong>.Nodes not read by the screen reader evaluate to <strong>true</strong>.</p><p> Nodes labeled as <strong>no name</strong> are visible to a screen reader, but were not given a name label.</p></li></ul>',
             position: 'bottom',
           },
           {
@@ -208,7 +224,7 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
           },
         ];
         break;
-      case 'accessibility'://'AxTree'
+      case 'accessibility': //'AxTree'
         steps = [
           {
             title: 'Accessibility Tree',
@@ -240,7 +256,7 @@ export default class Tutorial extends Component<TutorialProps, TutorialState> {
           },
           {
             title: 'Viewing History Snapshot',
-            element: '.display',//document.querySelectorAll('.snapshotNode')[0]
+            element: '.display', //document.querySelectorAll('.snapshotNode')[0]
             intro:
               'Each node will represent a snapshot in the page. <ul><li>A single snapshot will show as a node while multiple snapshots will be represented as a timeline.</li><li>Highlighting over one will show any state changes compared to the previous snapshot. </li><li>Clicking a node will set the snapshot as the current one.</li></ul>',
             position: 'top',
